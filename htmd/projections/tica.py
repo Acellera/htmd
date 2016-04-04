5fe89ae73a88de30b92ce3146c1b92732901f21c
Stefan Doerr <sdoerr@loro.imim.es>
2016-04-04 15:19:25 +0200

    reverted a tica change as the old pyemma does not support it

--- htmd/projections/tica.py
+++ htmd/projections/tica.py
@@ -79,7 +79,8 @@ class TICA(object):
         >>> dataTica = tica.project(5)
         """
         if ndim is not None:
-            self.tic.set_params(dim=ndim)
+            self.tic._dim = ndim
+            # self.tic.set_params(dim=ndim) # Change to this in new pyEMMA version
 
         if isinstance(self.data, Metric):  # Doesn't project on correct number of dimensions
             proj = []
