73d6566319afcf02a8a24211c72151b1bd195598
Toni G <toni.giorgino@gmail.com>
2016-04-06 13:19:44 +0200

    refactored residuedata

--- htmd/proteinpreparation/systempreparation.py
+++ htmd/proteinpreparation/systempreparation.py
@@ -5,6 +5,8 @@ import random
 
 from htmd.molecule.molecule import Molecule
 
+from htmd.proteinpreparation.residuedata import ResidueData
+
 # Trying the solution http://stackoverflow.com/questions/16981921/relative-imports-in-python-3
 # instead of relative imports, which fail when run as a script
 import sys
@@ -21,109 +23,23 @@ random.seed(2016)
 logger = logging.getLogger(__name__)
 
 
-# A generic container so I can set arbitrary properties
-class SystemPreparationData:
-    pass
 
 
-# Define a type for holding information on residues decisions
-class ResiduesInfo:
-    """ Class to hold results of the system preparation and optimization steps.
+def _createMolecule(name, resname, chain, resid, insertion, coords, segid, elements):
+    numAtoms = len(name)
+    mol = Molecule()
+    mol.empty(numAtoms)
 
-    ResiduesInfo contains the results of an optimization operation, notably, for each residue name, id, and chain, the
-    corresponding pKa and protonation state.
+    mol.name = np.array(name, dtype=mol._pdb_fields['name'])
+    mol.resname = np.array(resname, dtype=mol._pdb_fields['resname'])
+    mol.chain = np.array(chain, dtype=mol._pdb_fields['chain'])
+    mol.resid = np.array(resid, dtype=mol._pdb_fields['resid'])
+    mol.insertion = np.array(insertion, dtype=mol._pdb_fields['insertion'])
+    mol.coords = np.array(np.atleast_3d(np.vstack(coords)), dtype=mol._pdb_fields['coords'])
+    mol.segid = np.array(segid, dtype=mol._pdb_fields['segid'])
+    mol.element = np.array(elements, dtype=mol._pdb_fields['element'])
+    return mol
 
-    Examples
-    --------
-    >> tryp = Molecule("3PTB")
-    >> tryp_op, info = systemPreparation(tryp, returnDetails=True)
-    >> ri=info.residuesInfo
-    >> ri.pKa[ri.resid==189]
-
-    Properties
-    ----------
-
-    resid : np.ndarray
-        Residue ID
-    resname : np.ndarray
-        Residue name, as per the original PDB
-    chain : np.ndarray
-        Chain
-    pKa : np.ndarray
-        pKa value computed by propKa
-    protonation : np.ndarray
-        Forcefield-independent protonation code
-    patches : np.ndarray
-        Additional information (may change)
-
-    """
-    _residuesinfo_fields = {
-        'resid': np.int,
-        'resname': object,
-        'chain': object,
-        'pKa': np.float32,
-        'protonation': object,
-        'patches': object
-    }
-
-    def __init__(self):
-        for k in self._residuesinfo_fields:
-            self.__dict__[k] = np.zeros(0, dtype=self._residuesinfo_fields[k])
-
-    def __str__(self):
-        n=len(self.resid)
-        r=""
-        for i in range(n):
-            r += "%4s %4d %1s : pKa=%f, state=%s, patches=%s\n" % (
-                self.resname[i],
-                self.resid[i],
-                self.chain[i],
-                self.pKa[i],
-                self.protonation[i],
-                self.patches[i])
-        return r
-
-    def __repr__(self):
-        return self.__str__()
-
-
-    def _findRes(self, a_resid, a_resname, a_chain):
-        mask = (self.resid == a_resid) & (self.resname == a_resname) & (self.chain == a_chain)
-        assert (sum(mask) <= 1)
-        if sum(mask) == 0:
-            self.resid = np.append(self.resid, a_resid)
-            self.resname = np.append(self.resname, a_resname)
-            self.chain = np.append(self.chain, a_chain)
-            self.protonation = np.append(self.protonation, "UNK")
-            self.pKa = np.append(self.pKa, np.NaN)
-            self.patches = np.append(self.patches, "")
-            pos = len(self.resid) - 1
-        else:
-            pos = np.argwhere(mask)
-        return pos
-
-
-    # residue is e.g. pdb2pqr.src.aa.ILE
-    def setProtonation(self, residue, protonation):
-        logger.debug("setProtonation %s %s" % (residue,protonation))
-        pos = self._findRes(residue.resSeq, residue.name, residue.chainID)
-        self.protonation[pos] = protonation
-
-
-    # TODO this should actually append to a list
-    def appendPatches(self, residue, patch):
-        pos = self._findRes(residue.resSeq, residue.name, residue.chainID)
-        self.patches[pos] += patch + "/"
-
-
-    def setPKAs(self, pka_molecule):
-        for grp in pka_molecule.conformations['AVR'].groups:
-            resname=grp.residue_type
-            resid=grp.atom.resNumb
-            chain=grp.atom.chainID
-            pka=grp.pka_value
-            pos = self._findRes(resid, resname, chain)
-            self.pKa[pos]=pka
 
 
 
@@ -182,8 +98,7 @@ def systemPreparation(mol_in,
     Molecule
         the molecule titrated and optimized. The molecule object contains an additional attribute,
     SystemPreparationData, which contains these attributes:
-        - residuesInfo : a table of residues with the corresponding protonations and pKa
-        - ... other properties of lesser interest
+        - residueData : ResidueData a table of residues with the corresponding protonations and pKa
 
 
     Examples
@@ -232,14 +147,14 @@ def systemPreparation(mol_in,
     # to be "parse". Then I pick a convenient ffout.
 
     # Relying on defaults
-    header, lines, missedligands, pdb2pqr_protein = runPDB2PQR(pdblist, "parse", ph=pH,
-                                                               ffout="amber",
-                                                               verbose=verbose,
+    header, lines, missedLigands, pdb2pqr_protein = runPDB2PQR(pdblist,
+                                                               ph=pH, verbose=verbose,
+                                                               ff="parse", ffout="amber",
                                                                propkaOptions=propka_opts)
     tmpin.close()
 
     # Diagnostics
-    for missedligand in missedligands:
+    for missedligand in missedLigands:
         logger.warning("The following residue has not been optimized: " + missedligand)
 
     # Here I parse the returned protein object and recreate a Molecule,
@@ -256,7 +171,7 @@ def systemPreparation(mol_in,
     segids = []
     elements = []
 
-    resinfo = ResiduesInfo()
+    resData = ResidueData()
 
     for residue in pdb2pqr_protein.residues:
         if 'ffname' in residue.__dict__:
@@ -268,11 +183,11 @@ def systemPreparation(mol_in,
         else:
             curr_resname = residue.name
 
-        resinfo.setProtonation(residue, curr_resname)
+        resData.setProtonation(residue, curr_resname)
 
         if 'patches' in residue.__dict__:
             for patch in residue.patches:
-                resinfo.appendPatches(residue, patch)
+                resData.appendPatches(residue, patch)
                 if patch != "PEPTIDE":
                     logger.info("Residue %s has patch %s set" % (residue, patch))
 
@@ -286,47 +201,31 @@ def systemPreparation(mol_in,
             segids.append(atom.segID)
             elements.append(atom.element)
 
-    resinfo.setPKAs(pdb2pqr_protein.pka_molecule)
+    resData.setPKAs(pdb2pqr_protein.pka_molecule)
 
     mol_out = _createMolecule(name, resname, chain, resid, insertion, coords, segids, elements)
 
-    systemPreparationData = SystemPreparationData()
-    systemPreparationData.pdb2pqr_protein = pdb2pqr_protein
-    systemPreparationData.pka_protein = pdb2pqr_protein.pka_molecule
-    systemPreparationData.pka_dict = pdb2pqr_protein.pkadic
-    systemPreparationData.residuesInfo = resinfo
+    resData.pdb2pqr_protein = pdb2pqr_protein
+    resData.pka_protein = pdb2pqr_protein.pka_molecule
+    resData.pka_dict = pdb2pqr_protein.pkadic
+    resData.missedLigands=missedLigands
 
     logger.info("Returning.")
 
     if returnDetails:
-        return mol_out, systemPreparationData
+        return mol_out, resData
     else:
         return mol_out
 
 
 
 
-def _createMolecule(name, resname, chain, resid, insertion, coords, segid, elements):
-    numAtoms = len(name)
-    mol = Molecule()
-    mol.empty(numAtoms)
-
-    mol.name = np.array(name, dtype=mol._pdb_fields['name'])
-    mol.resname = np.array(resname, dtype=mol._pdb_fields['resname'])
-    mol.chain = np.array(chain, dtype=mol._pdb_fields['chain'])
-    mol.resid = np.array(resid, dtype=mol._pdb_fields['resid'])
-    mol.insertion = np.array(insertion, dtype=mol._pdb_fields['insertion'])
-    mol.coords = np.array(np.atleast_3d(np.vstack(coords)), dtype=mol._pdb_fields['coords'])
-    mol.segid = np.array(segid, dtype=mol._pdb_fields['segid'])
-    mol.element = np.array(elements, dtype=mol._pdb_fields['element'])
-    return mol
-
 
 # A test method
 if __name__ == "__main__":
     tryp = Molecule('3PTB')
 
-    tryp_op = systemPreparation(tryp, pH=1.0,verbose=True)
+    tryp_op = systemPreparation(tryp, pH=1.0)
     tryp_op.write('proteinpreparation-test-main-ph-1.pdb')
 
     tryp_op, prepData = systemPreparation(tryp,returnDetails=True)
