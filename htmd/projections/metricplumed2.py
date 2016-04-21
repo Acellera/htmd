bc93665c1e81ecbd0baa742837bd902baafb9554
Toni G <toni.giorgino@gmail.com>
2016-04-21 13:37:06 +0200

    next gen plumed

--- htmd/projections/metricplumed2.py
+++ htmd/projections/metricplumed2.py
@@ -12,6 +12,7 @@ import tempfile
 
 logger = logging.getLogger(__name__)
 
+cvcounter = 1
 
 # Local utility functions --------------------------------------------------------
 
@@ -65,6 +66,7 @@ class PlumedCV(PlumedStatement):
     lig: COM ATOMS=1632,1633,1634,1635,1636,1637,1638,1639,1640
     protCA: COM ATOMS=2,10,17,21,25,37,44,50,54,59,67,74,81,88,100,109,116,122,130,138,144,148,160,170,181,187,191,195,201,209,217,225,231,240,254,261,268,274,279,284,294,300,312,321,327,331,339,348,355,366,374,378,387,395,403,411,419,426,433,442,446,454,463,472,483,491,497,502,508,517,523,531,538,548,555,561,573,581,587,595,602,610,618,626,634,642,650,658,666,675,683,692,698,703,708,714,722,730,736,747,754,759,765,773,779,787,794,801,807,813,818,824,829,833,840,849,855,863,871,877,881,895,899,907,914,923,929,935,939,946,952,964,971,979,986,994,1003,1009,1017,1026,1031,1038,1046,1054,1060,1068,1074,1080,1086,1095,1101,1106,1118,1125,1129,1138,1146,1153,1159,1167,1175,1186,1192,1197,1201,1213,1221,1230,1234,1238,1247,1255,1261,1267,1276,1280,1288,1294,1298,1302,1309,1316,1323,1329,1335,1339,1348,1356,1365,1369,1377,1384,1390,1404,1408,1414,1418,1424,1429,1438,1447,1455,1464,1471,1475,1482,1494,1501,1510,1517,1523,1531,1543,1550,1556,1570,1578,1587,1596,1603,1611,1616,1622,1631
     dist: DISTANCE ATOMS=protCA,lig
+    >>> PlumedCV("GYRATION", "rgyr3", ATOMS=m)
     """
 
     def __init__(self, cv, label, verbatim=None, **kw):
@@ -76,25 +78,47 @@ class PlumedCV(PlumedStatement):
 
         for k in self.args:
             v = self.args[k]
+            # Check all possible RHS types...
             # If the arg is a Plumed group, add it to prereq and replace by label
             if isinstance(v, PlumedGenericGroup):
                 self.prereq.append(v)
                 self.args[k] = v.label
+            # If the args is a Molecule object, convert to a PlumedGroup
+            elif isinstance(v, Molecule):
+                tmpGrp=PlumedGroup(label=None,mol=v,sel="all")
+                self.prereq.append(tmpGrp)
+                self.args[k] = tmpGrp.label
+            # Boolean, as in PBC=True, is ok
+            elif isinstance(v, bool):
+                pass
+            # String is ok
+            elif isinstance(v, str):
+                pass
             # Ditto if it is a list-like object, plus expand with commas
-            elif not isinstance(v, str):
+            elif hasattr(v, '__iter__'):
                 for l in range(len(v)):
                     le = v[l]
                     if isinstance(le, PlumedGenericGroup):
                         self.prereq.append(le)
                         v[l] = le.label
+                    elif isinstance(le, Molecule):
+                        tmpGrp = PlumedGroup(label=None, mol=le, sel="all")
+                        self.prereq.append(tmpGrp)
+                        v[l] = tmpGrp.label
+                    elif isinstance(le, str):
+                        pass # already a string
+                    else:
+                        raise TypeError("Unexpected type passed at argument (list): " + k)
                 self.args[k] = ",".join(v)
+            else:
+                raise TypeError("Unexpected type passed at argument: " + k)
 
     def __repr__(self):
         r = ""
 
         # Prerequisites (uniquely)
-        for p in sorted(set(self.prereq)):
-            r = r + str(p) + "\n"
+        for p in sorted([str(i) for i in set(self.prereq)]):
+            r = r + p + "\n"
 
         # Label
         r = r + self.label + ": " + self.cv + " "
@@ -117,12 +141,17 @@ class PlumedGenericGroup(PlumedStatement):
     """ Abstract class from which PLUMED groups are inherited. Do not use directly. """
 
     def __init__(self, mol, label, sel, type=""):
+        global cvcounter
         al = mol.get("serial", sel)
         al = list(al)
-        self.label = label
+        if label:
+            self.label = label
+        else:
+            self.label = "lab_" + str(cvcounter)
+            cvcounter = cvcounter + 1
         self.mol = mol
         self.sel = sel
-        self.code = "%s: %s ATOMS=%s" % (label, type, ",".join(map(str, al)))
+        self.code = "%s: %s ATOMS=%s" % (self.label, type, ",".join(map(str, al)))
 
     def __repr__(self):
         return self.code
@@ -136,7 +165,7 @@ class PlumedGroup(PlumedGenericGroup):
     mol: Molecule
         The molecule
     label: str
-        The label assigned to the group
+        The label assigned to the group.
     sel: str
         The atom selection defining the group
 
