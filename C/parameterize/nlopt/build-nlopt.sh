8da821fba0fc41be6ddcf3c7036a2ef4c7465419
M J Harvey <m.j.harvey@acellera.com>
2016-04-22 13:50:11 +0100

    build diddle

--- C/parameterize/nlopt/build-nlopt.sh
+++ C/parameterize/nlopt/build-nlopt.sh
@@ -3,6 +3,10 @@ mkdir -p nlopt/build
 cd nlopt/build
 tar -zxvf ../nlopt-2.4.2.tar.gz
 cd nlopt*
-./configure --prefix="$PWD/../.." CXX=$CC CC=$CC FC=$FC
+echo "CXX=$CC"
+echo "CC=$CC"
+echo "FC=$FC"
+./configure --prefix="$PWD/../.." CXX=$CC CC=$CC FC=$FC || cat config.log
+
 make && make install
 
