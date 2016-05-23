6f5f73bd21a6491011ee12d45d6dfe398bda2c57
Toni G <toni.giorgino@gmail.com>
2016-05-23 12:07:44 +0200

    implemented diffMolecule

--- htmd/util.py
+++ htmd/util.py
@@ -7,6 +7,7 @@ import tempfile
 import logging
 import requests
 import os
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +19,61 @@ def tempname(suffix='', create=False):
         file = tempfile.NamedTemporaryFile(delete=True, suffix=suffix)
     file.close()
     return file.name
+
+
+def diffMolecules(mol1, mol2, sel=None):
+    """Check that name, resname, resid, insertion codes match between two molecules.
+
+    Coordinates are not checked.
+
+    Parameters
+    ----------
+    mol1 : Molecule
+        first structure to compare
+    mol2 : Molecule
+        second structure to compare
+    sel: str
+        compare after filtering with the given atomselection
+
+    Returns
+    -------
+    diff: list
+        a list of differences, as human-readable strings (empty if structures are equal).
+
+    Examples
+    --------
+    >>> m=Molecule("3PTB")
+    >>> m2=m.copy()
+    >>> m2.set("resname","HIE","resid 91")
+    >>> diffMolecules(m,m2,sel="name CA")
+    ['CA   HIS    91     vs   CA   HIE    91  ']
+    """
+
+    from numpy import logical_and as l_and
+
+    diff = []
+
+    m1 = mol1.copy()
+    m1.filter(sel)
+    m2 = mol2.copy()
+    m2.filter(sel)
+
+    eq_name = np.equal(m1.name, m2.name)
+    eq_resid = np.equal(m1.resid, m2.resid)
+    eq_resname = np.equal(m1.resname, m2.resname)
+    eq_insertion = np.equal(m1.insertion, m2.insertion)
+
+    eq_all = l_and(eq_name,
+                   l_and(eq_resid,
+                         l_and(eq_resname, eq_insertion)))
+
+    neq = np.logical_not(eq_all).nonzero()
+    for i in neq[0]:
+        diff.append("{:4s} {:4s} {:4d} {:1s}   vs   {:4s} {:4s} {:4d} {:1s}".format(
+            m1.name[i], m1.resname[i], m1.resid[i], m1.insertion[i],
+            m2.name[i], m2.resname[i], m2.resid[i], m2.insertion[i],
+        ))
+    return diff
 
 
 def opm(pdb):
@@ -42,7 +98,7 @@ def opm(pdb):
     --------
     >>> mol, thickness = opm("1z98")
     >>> mol.numAtoms
-    10272
+    7902
     >>> thickness
     28.2
 
@@ -68,7 +124,7 @@ def opm(pdb):
     os.unlink(tempfile)
 
     hs = h.split()
-    thickness = 2.0*float(hs[-1])
+    thickness = 2.0 * float(hs[-1])
 
     return mol, thickness
 
@@ -92,7 +148,7 @@ def issueDeprecationWarning(msg):
         _issuedDeprecationWarnings[caller] = 1
         logger.warning(
             "Deprecation warning (%s). The function is obsolete and will be removed in a future version! Additional info: %s" % (
-            caller, msg))
+                caller, msg))
 
 
 def _testDeprecation():
@@ -102,8 +158,9 @@ def _testDeprecation():
 
 if __name__ == "__main__":
     from htmd import *
-    import doctest
-
-    doctest.testmod()
 
     _testDeprecation()
+
+    import doctest
+    doctest.testmod()
+
