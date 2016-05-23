bbf72b735f2a0efa47369251cd4d629e04002686
M J Harvey <m.j.harvey@acellera.com>
2016-05-23 22:55:42 +0100

    diddle

--- tests/tests/check_static/run.sh
+++ tests/tests/check_static/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+which ldd
+if [ "$?" == "1" ]; then
+	# No ldd -- infer that this is Travis OS X
+	exit 0
+fi
+
 RET=0
 for T in  $(dirname $(which parameterize))/../lib/*/*/htmd/parameterize/share/bin/* ; do
 	ldd $T | grep -q "not a dynamic executable"
