426a9de579e5974417e6b9d2a25e9127c6edd810
M J Harvey <m.j.harvey@acellera.com>
2016-05-23 22:23:16 +0100

    diddle

--- tests/tests/check_static/run.sh
+++ tests/tests/check_static/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 RET=0
-for T in ../../../htmd/parameterize/share/bin/*; do
+for T in  $(dirname $(which parameterize))/../lib/*/*/htmd/parameterize/share/bin/* ; do
 	ldd $T | grep -q "not a dynamic executable"
 	if [ "$?" == "1" ]; then
 		RET=1
