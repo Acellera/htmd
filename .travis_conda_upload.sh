207185caddd06e5f841dc39014f34c3c37f2ab1b
M J Harvey <m.j.harvey@acellera.com>
2016-05-30 22:07:15 +0100

    fix upload if not crosscompiling

--- .travis_conda_upload.sh
+++ .travis_conda_upload.sh
@@ -16,11 +16,12 @@ fi
 
 export CHANNEL=acellera
 echo "Uploading to channel: $CHANNEL"
-anaconda -t $ANACONDA_TOKEN upload  $HOME/miniconda/conda-bld/*-64/htmd-[0-9]*.tar.bz2 -u $CHANNEL
 
 if [ "$CROSS_COMPILE" == "1" ]; then
     conda convert -f -p win-64 $HOME/miniconda/conda-bld/linux-64/htmd-[0-9]*.tar.bz2
     anaconda -t $ANACONDA_TOKEN upload win-64/htmd-[0-9]*.tar.bz2 -u $CHANNEL
+else
+		anaconda -t $ANACONDA_TOKEN upload  $HOME/miniconda/conda-bld/*-64/htmd-[0-9]*.tar.bz2 -u $CHANNEL
 fi
 
 
