cd97564cf2d7e103e9f0e394752b495e9beb3279
Stefan Doerr <stefan.doerr@upf.edu>
2016-06-08 11:50:34 +0200

    ChangeLog: fixed bug which reading a psf overwrote coordinates

--- htmd/molecule/molecule.py
+++ htmd/molecule/molecule.py
@@ -647,7 +647,11 @@ class Molecule:
             type = type.lower()
 
         if (type is None and firstfile.endswith(".psf")) or type == "psf":
+            # TODO: Check for validity when loading a PSF after a PDB and vice versa
             con = PSFread(filename)
+            oldcoords = []
+            if len(self.coords) != 0:
+                oldcoords = self.coords
             self.empty(len(con.serial))  # initialize all arrays as empty
             self.serial = con.serial
             self.name   = con.atomname
@@ -658,6 +662,8 @@ class Molecule:
             self.charge = numpy.asarray(con.charges, dtype=np.float32)
             self.masses = numpy.asarray(con.masses, dtype=np.float32)
             self.bonds = numpy.asarray(con.bonds, dtype=np.uint32)
+            if len(oldcoords) != 0:
+                self.coords = oldcoords
         elif (type is None and (
             firstfile.endswith(".prm") or firstfile.endswith(".prmtop"))) or type == "prmtop" or type == "prm":
             con = PRMTOPread(filename)
