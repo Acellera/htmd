1f5232615f6827285cb2ef990ab28db11fa1e5f4
j3mdamas <jmdamas@gmail.com>
2016-06-09 13:11:35 +0200

    name, refactoring, adapted to new Parameterization behaviour

--- htmd/parameterization/cli.py
+++ htmd/parameterization/cli.py
@@ -1,28 +1,23 @@
-# Suppress the unhelpful warning  that matplotlib emits
-import warnings
-warnings.filterwarnings("ignore")
-
-import matplotlib as mpl
-
-mpl.use('Agg')
-
 import os
 import shutil
 import re
 import sys
 from htmd.parameterization.configuration import ParameterizationConfig
 from htmd.parameterization.command import Command
-
 from htmd.parameterization.parameterization import Parameterization
 import logging
+import warnings
+import matplotlib as mpl
 
+# Suppress the unhelpful warning  that matplotlib emits
+warnings.filterwarnings("ignore")
+mpl.use('Agg')
 
-# from htmdx.cli import *
 
 def syntax():
     print("")
-    print(" Acellera Small Molecule Parameterisation 2016 (c) \n")
-    print("  Syntax: parameterization [args]:\n")
+    print(" Acellera Small Molecule Parameterization 2016 (c) \n")
+    print("  Syntax: parameterize [args]:\n")
     print("    --input [inputfile]      : Specify the input file. Default value 'input'")
     print("    --resume [job]           : Specify name of a job to resume")
     print("    --command ([command])    : Show help for an input-file command")
@@ -44,74 +39,72 @@ def main_parameterize():
     mpl.use('Agg')
 
     input_file = None
-    device = None
+    # device = None
     verbose = False
-    platform = None
+    # platform = None
     debug = False
     list_jobs = False
     rename_file = None
     job_to_delete = False
     resume = None
-    cache_prefix = None
+    # cache_prefix = None
     # check_registration( product="parameterization" )
 
     a = 1
     try:
-        while (a < len(sys.argv)):
-            if (sys.argv[a] == "--rename"):
+        while a < len(sys.argv):
+            if sys.argv[a] == "--rename":
                 if a < len(sys.argv) - 1:
                     rename_file = sys.argv[a + 1]
-                    a = a + 1
+                    a += 1
                 else:
                     raise NameError()
-            elif (sys.argv[a] == "--cache"):
+            elif sys.argv[a] == "--cache":
                 if a < len(sys.argv) - 1:
                     cache_prefix = sys.argv[a + 1]
                     Parameterization.set_prefix(cache_prefix)
-                    a = a + 1
+                    a += 1
                 else:
                     raise NameError()
-
-            elif (sys.argv[a] == "--torsions"):
+            elif sys.argv[a] == "--torsions":
                 if a < len(sys.argv) - 1:
                     try:
                         tt = (Parameterization.listDihedrals(sys.argv[a + 1]))
                         tt = tt[1]
                         for x in tt:
                             for y in x:
-                                print("%5s" % (y), end="")
+                                print("%5s" % y, end="")
                             print("")
                     except NameError as e:
                         print(str(e))
                     sys.exit(0)
                 else:
                     raise NameError()
-
-            elif (sys.argv[a] == "--input"):
+            elif sys.argv[a] == "--input":
                 if a < len(sys.argv) - 1:
                     input_file = sys.argv[a + 1]
-                    a = a + 1
+                    a += 1
                 else:
                     raise NameError()
-            elif (sys.argv[a] == "--resume"):
+            elif sys.argv[a] == "--resume":
                 if a < len(sys.argv) - 1:
                     resume = sys.argv[a + 1]
-                    a = a + 1
+                    a += 1
                 else:
                     raise NameError()
             elif sys.argv[a] == "--license":
-                #				show_license( product="parameterization" )
+                # show_license( product="parameterization" )
                 sys.exit(0)
-            elif (sys.argv[a] == "--debug"):
+            elif sys.argv[a] == "--debug":
                 debug = True
-            elif (sys.argv[a] == "--list"):
+            elif sys.argv[a] == "--list":
                 list_jobs = True
             elif (sys.argv[a] == "--delete") and (a < len(sys.argv) - 1):
                 job_to_delete = sys.argv[a + 1]
-                a = a + 1
-            elif (sys.argv[a] == "--verbose"):
+                a += 1
+            elif sys.argv[a] == "--verbose":
                 verbose = True
-            elif (sys.argv[a] == "--command"):
+            elif sys.argv[a] == "--command":
                 if a < len(sys.argv) - 1:
                     Command.help(sys.argv[a + 1])
                     sys.exit(1)
@@ -125,7 +118,6 @@ def main_parameterize():
                 sys.exit(0)
             else:
                 print("Unparsed : " + sys.argv[a])
-
             a += 1
     except NameError as e:
         if debug:
@@ -165,8 +157,7 @@ def main_parameterize():
         syntax()
         sys.exit(0)
 
-    print("\n === Parameterise 2016 ===\n")
-    print("      (c) Acellera ")
+    print("\n === Acellera Small Molecule Parameterization 2016 (c) === \n")
     try:
         if input_file:
             config = ParameterizationConfig(config=input_file, check=True)
@@ -191,22 +182,22 @@ def main_parameterize():
 
     try:
         p = Parameterization(config=config, name=resume)
-        (fp, names) = p.plotDihedrals(show=False)
-        pp = p.getParameters()
-        for i in range(len(fp)):
-            shutil.copyfile(fp[i], "torsion-potential-" + re.sub(" ", "-", names[i]) + ".svg")
-        shutil.copyfile(pp['RTF'], "mol.rtf")
-        shutil.copyfile(pp['PRM'], "mol.prm")
-        shutil.copyfile(pp['PDB'], "mol.pdb")
+        p.plotDihedrals(show=False)
+        # for i in range(len(fp)):
+        #     shutil.copyfile(fp[i], "torsion-potential-" + re.sub(" ", "-", names[i]) + ".svg")
+        p.getParameters()
+        # shutil.copyfile(pp['RTF'], "mol.rtf")
+        # shutil.copyfile(pp['PRM'], "mol.prm")
+        # shutil.copyfile(pp['PDB'], "mol.pdb")
 
     except (NameError, ValueError) as e:
-        print("\n Failed to run parameterisation : " + str(e) + "\n")
+        print("\n Failed to run parameterization : " + str(e) + "\n")
         if debug:
             raise e
         sys.exit(3)
 
     except KeyboardInterrupt as e:
-        print("\n Terminating at user request before parameterisation started\n")
+        print("\n Terminating at user request before parameterization started\n")
         if debug:
             raise e
         sys.exit(5)
