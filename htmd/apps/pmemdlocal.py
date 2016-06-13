8f6692969164fa6425cb31dbc38e68b22455f5f2
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-06-13 13:00:14 +0100

    Finished up the constructor
    Similar to AcemdLocal.py but it looks for the pmemd.cuda_SPFP binary.
    Users can always specify any other type of pmemd binary they might want
    to use.

--- htmd/apps/pmemdlocal.py
+++ htmd/apps/pmemdlocal.py
@@ -1,10 +1,56 @@
 from htmd.apps.app import App
+from shutil import which, move
+from subprocess import call, check_output, CalledProcessError
+import threading
+import logging
+import os
+logger = logging.getLogger(__name__)
 
 
 class PmemdLocal(App):
 
-    def __init__(self, pmemd=None, ngpus=None, devices=None, datadir=None):
-        pass
+    def __init__(self, pmemd_cuda=None, ngpus=None, devices=None, datadir=None):
+        self.states = dict()
+        self.queue = queue.Queue()
+        self.threads = []
+        self.shutdown = False
+
+        if ngpus is not None and devices is not None:
+            raise ValueError('Parameters `ngpus` and `devices` are mutually exclusive.')
+
+        if ngpus is None and devices is None:
+            try:
+                devices = range(int(check_output("nvidia-smi -L | wc -l",
+                                shell=True).decode("ascii")))
+            except:
+                raise
+        elif ngpus is not None:
+            devices = range(ngpus)
+
+        if devices is None:
+            raise NameError("Could not determine which GPUs to use. "
+                            "Specify the GPUs with the `ngpus=` or `devices=` parameters")
+        else:
+            logger.info("Using GPU devices {}".format(','.join(map(str, devices))))
+
+        if not pmemd_cuda:
+            try:
+                # Try to find pmemd.cuda_SPFP in the path
+                pmemd_cuda = which("pmemd.cuda_SPFP", mode=os.X_OK)
+                if pmemd_cuda:
+                    logger.info("Found pmemd.cuda_SPFP at '" + pmemd_cuda + "'")
+            except:
+                raise NameError("Cannot find 'pmemd.cuda_SPFP' in the PATH. Set its location with the 'pmemd_cuda=' named argument")
+
+        if not os.access(pmemd_cuda, os.X_OK):
+            raise NameError("pmemd_cuda file '" + pmemd_cuda + "' not executable")
+
+        self.threads = []
+        for d in devices:
+            t = threading.Thread(target=run_job, args=(self, d, pmemd_cuda, datadir))
+            t.daemon = True
+            t.start()
+            self.threads.append(t)
 
     def retrieve(self):
         pass
