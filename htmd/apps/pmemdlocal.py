e647b461cf64a4eaaaaba5968fb8b6702941ebae
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-06-13 12:27:26 +0100

    Initial test of PmemdLocal class

---
+++ htmd/apps/pmemdlocal.py
@@ -0,0 +1,16 @@
+from htmd.apps.app import App
+
+
+class PmemdLocal(App):
+
+    def __init__(self, pmemd=None, ngpus=None, devices=None, datadir=None):
+        pass
+
+    def retrieve(self):
+        pass
+
+    def submit(self, dirs):
+        pass
+
+    def inprogress(self):
+        pass
