1c11941cdfb33cefaafeda77d3870775d89a465c
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-06-14 14:43:06 +0100

    stdout flush
    Trying to figure out what is going on

--- htmd/apps/pmemdlocal.py
+++ htmd/apps/pmemdlocal.py
@@ -95,6 +95,8 @@ def run_job(obj, ngpu, pmemd_cuda, datadir, system_name):
                 logger.info("Running " + path + " on GPU device " + str(ngpu))
                 logger.info("Binary location: {}".format(pmemd_cuda))
                 print("System Name: {}".format(system_name))
+                import sys
+                sys.stdout.flush()
                 obj.running(path)
                 # TODO: Pre-production steps
                 cmd = """cd {} && {} -O -i 05_Prod.in -o {}.out \\
