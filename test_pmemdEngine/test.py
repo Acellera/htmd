fdc4685c87cad6c5cae801caaa398864c3d4e9bf
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-06-14 11:51:47 +0100

    Updated the test file

--- test_pmemdEngine/test.py
+++ test_pmemdEngine/test.py
@@ -1,10 +1,10 @@
 import htmd
-adapt = htmd.AdaptiveRun()
+adapt = htmd.AdaptiveRun(inputpath='./test_pmemdEngine')
 adapt.nmin = 2
 adapt.nmax = 3
 adapt.nepochs = 2
 adapt.ticadim = 0
 adapt.metricsel1 = 'name CA'
-adapt.generatorspath = './'
-adapt.app = htmd.apps.pmemdlocal.PmemdLocal()
+adapt.generatorspath = './test_pmemdEngine'
+adapt.app = htmd.apps.pmemdlocal.PmemdLocal(pmemd_cuda='/usr/local/amber/bin/pmemd.cuda_SPFP')
 adapt.run()
