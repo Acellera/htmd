f6ed4f7fb64faef133bd9e901989577cf813763c
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-06-15 15:14:33 +0100

    Update test.py
    Manually setting the class atributes to test them out

--- test_pmemdEngine/test.py
+++ test_pmemdEngine/test.py
@@ -7,13 +7,16 @@ from glob import glob
 for directory_i in glob('./test_pmemdEngine/*_test_pmemdEngine'):
     shutil.rmtree(directory_i)
 
-adapt = htmd.AdaptiveRun(inputpath='./test_pmemdEngine')
-adapt.nmin = 2
-adapt.nmax = 3
-adapt.nepochs = 2
-adapt.ticadim = 0
-adapt.metricsel1 = 'name CA'
-adapt.generatorspath = './test_pmemdEngine'
+adapt = htmd.AdaptiveRun()
 adapt.app = htmd.apps.pmemdlocal.PmemdLocal(
     pmemd_cuda='/usr/local/amber/bin/pmemd.cuda_SPFP')
+adapt.project = 'Test'
+adapt.nmin = 4
+adapt.nmax = 4
+adapt.nepochs = 10
+adapt.inputpath = '~/test_htmd'
+adapt.generatorspath = '~/test_htmd'
+adapt.metricsel1 = 'name CA'
+adapt.datapath = '~/test_htmd'
+adapt.ticadim = 0
 adapt.run()
