82b1acabbf1003eb12d26dc22aa8c11ffc1d6e8a
Gil <g.f.hoben@gmail.com>
2016-06-16 14:22:07 +0100

    some changes in test.py to test generator path

--- test_pmemdEngine/test.py
+++ test_pmemdEngine/test.py
@@ -6,6 +6,8 @@ from glob import glob
 # delete previous test
 for directory_i in glob('./test_pmemdEngine/*_test_pmemdEngine'):
     shutil.rmtree(directory_i)
+#for directory_i in glob('./test_pmemdEngine/generator*'):
+#	shutil.rmtree(directory_i)
 
 # run adaptive sampling with MSM
 adapt = htmd.AdaptiveRun()
@@ -16,7 +18,10 @@ adapt.nmin = 4
 adapt.nmax = 4
 adapt.nepochs = 10
 adapt.inputpath = 'test_pmemdEngine'
-adapt.generatorspath = 'test_pmemdEngine'
+adapt.generatorspath = ['test_pmemdEngine/generator1',
+						'test_pmemdEngine/generator2',
+						'test_pmemdEngine/generator3',
+						'test_pmemdEngine/generator4']
 adapt.metricsel1 = 'name CA'
 adapt.datapath = 'test_pmemdEngine'
 adapt.ticadim = 0
