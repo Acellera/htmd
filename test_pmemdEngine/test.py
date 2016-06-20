60427022c611f92f3b6c50e902ddb492e5c7c8b8
Gil <g.f.hoben@gmail.com>
2016-06-20 12:21:00 +0100

    fixed typos

--- test_pmemdEngine/test.py
+++ test_pmemdEngine/test.py
@@ -1,11 +1,10 @@
 import htmd
 import shutil
 from glob import glob
-from htmd
 import os
 
 # delete previous test
-for directory_i in glob(os.join.path(htmd.home(),'./test_pmemdEngine/*_test_pmemdEngine')):
+for directory_i in glob(os.path.join(htmd.home(),'./test_pmemdEngine/*_test_pmemdEngine')):
     shutil.rmtree(directory_i)
 
 # Create Equilibration Protocol
@@ -14,8 +13,8 @@ test_equil = Equilibration()
 # for now let us leave it as is and create a folder with all that we need
 # to run this test we need Test_Protocol with a prmtop and rst files
 # and a (empty) Test_Protocol_out directory, both in htmd.home()
-test_equil.write(os.join.path(htmd.home(), 'Test_Protocol'),
-				 os.join.path(htmd.home(),'Test_Protocol_out'))
+test_equil.write(os.path.join(htmd.home(), 'Test_Protocol'),
+				 os.path.join(htmd.home(),'Test_Protocol_out'))
 
 # run adaptive sampling with MSM
 adapt = htmd.AdaptiveRun()
@@ -26,7 +25,7 @@ adapt.nmin = 4
 adapt.nmax = 4
 adapt.nepochs = 2
 adapt.inputpath = 'test_pmemdEngine'
-adapt.generatorspath = os.join.path(htmd.home(),'Test_Protocol_out')
+adapt.generatorspath = os.path.join(htmd.home(),'Test_Protocol_out')
 adapt.metricsel1 = 'name CA'
 #adapt.datapath = 'test_pmemdEngine'
 #adapt.ticadim = 0
