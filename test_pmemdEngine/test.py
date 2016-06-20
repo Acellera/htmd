b0756192c169f67c6c34194e980c89b43149e06f
Gil <g.f.hoben@gmail.com>
2016-06-20 12:15:25 +0100

    updated test.py

--- test_pmemdEngine/test.py
+++ test_pmemdEngine/test.py
@@ -1,23 +1,34 @@
 import htmd
 import shutil
 from glob import glob
+from htmd
+import os
 
 # delete previous test
-for directory_i in glob('./test_pmemdEngine/*_test_pmemdEngine'):
+for directory_i in glob(os.join.path(htmd.home(),'./test_pmemdEngine/*_test_pmemdEngine')):
     shutil.rmtree(directory_i)
+
+# Create Equilibration Protocol
+from htmd.protocols.Amber_protocols.AmberEquilibration import Equilibration
+test_equil = Equilibration()
+# for now let us leave it as is and create a folder with all that we need
+# to run this test we need Test_Protocol with a prmtop and rst files
+# and a (empty) Test_Protocol_out directory, both in htmd.home()
+test_equil.write(os.join.path(htmd.home(), 'Test_Protocol'),
+				 os.join.path(htmd.home(),'Test_Protocol_out'))
 
 # run adaptive sampling with MSM
 adapt = htmd.AdaptiveRun()
 adapt.app = htmd.apps.pmemdlocal.PmemdLocal(
     pmemd_cuda='/usr/local/amber/bin/pmemd.cuda_SPFP')
-adapt.project = 'Test'
+#adapt.project = 'Test'
 adapt.nmin = 4
 adapt.nmax = 4
-adapt.nepochs = 10
+adapt.nepochs = 2
 adapt.inputpath = 'test_pmemdEngine'
-adapt.generatorspath = 'test_pmemdEngine'
+adapt.generatorspath = os.join.path(htmd.home(),'Test_Protocol_out')
 adapt.metricsel1 = 'name CA'
-adapt.datapath = 'test_pmemdEngine'
-adapt.ticadim = 0
-adapt.updateperiod = 1
+#adapt.datapath = 'test_pmemdEngine'
+#adapt.ticadim = 0
+#adapt.updateperiod = 1
 adapt.run()
