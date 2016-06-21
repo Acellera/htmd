273ac8fe25b9981032e991152da78236f623fe75
Gil <g.f.hoben@gmail.com>
2016-06-21 11:27:21 +0100

    Created two tests for both protocols
    Currently testing the production protocol, everything is running, next
    step is to make sure the algorithm runs

---
+++ test_pmemdEngine/ProdTest.py
@@ -0,0 +1,34 @@
+import htmd
+import shutil
+from glob import glob
+import os
+
+# delete previous test
+for directory_i in glob(os.path.join(htmd.home(), 'test_pmemdEngine/*_Test_Protocol_out')):
+    shutil.rmtree(directory_i)
+
+# Create Equilibration Protocol
+from htmd.protocols.Amber_protocols.AmberProduction import Production
+ProdTest = Production()
+ProdTest.amber.nstlim = 100000
+# for now let us leave it as is and create a folder with all that we need
+# to run this test we need Test_Protocol with a prmtop and rst files
+# and a (empty) Test_Protocol_out directory, both in htmd.home()
+ProdTest.write(os.path.join(htmd.home(), 'Test_Protocol'),
+                 os.path.join(htmd.home(), 'Test_Protocol_out'))
+
+# run adaptive sampling with MSM
+adapt = htmd.AdaptiveRun()
+adapt.app = htmd.apps.pmemdlocal.PmemdLocal(
+    pmemd_cuda='/usr/local/amber/bin/pmemd.cuda_SPFP')
+#adapt.project = 'Test'
+adapt.nmin = 2
+adapt.nmax = 4
+adapt.nepochs = 2
+adapt.inputpath = os.path.join(htmd.home(), 'test_pmemdEngine')
+adapt.generatorspath = os.path.join(htmd.home(), 'Test_Protocol_out')
+adapt.metricsel1 = 'name CA'
+#adapt.datapath = 'test_pmemdEngine'
+#adapt.ticadim = 0
+adapt.updateperiod = 3600
+adapt.run()
