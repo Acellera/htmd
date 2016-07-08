7f22e0ac5a10effef4d6a6fbfe0b20c012c945e5
Stefan Doerr <stefan.doerr@upf.edu>
2016-07-08 19:27:19 +0200

    extended the C code to support multiframe coordinates. now I can loop the frames in C as well if need be

--- htmd/projections/mindist/mindist_ext.c
+++ htmd/projections/mindist/mindist_ext.c
@@ -2,17 +2,26 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define X(atom, frame, nframes, nf3) atom*nf3+frame
+#define Y(atom, frame, nframes, nf3) atom*nf3+nframes+frame
+#define Z(atom, frame, nframes, nf3) atom*nf3+2*nframes+frame
+
 
 const int dim = 3;
+
+// Too slow calling a function
+//void get_coords(float* coords, int atom, int frame, int nframes, float* c){
+//    c[0] = coords[atom*3*nframes+0*nframes+frame];
+//    c[1] = coords[atom*3*nframes+1*nframes+frame];
+//    c[2] = coords[atom*3*nframes+2*nframes+frame];
+//}
 
 void mindist_ext(float* coords, int* groups1, int* groups2, int gn1, int gn2, int na, float* dist) {
     float mindist;
     int a,b,g1,g2,n1,n2,g1atm,g2atm;
-
-    //for (a=0; a<4; a++){
-    //    const float* coo2 = &coords[a*dim];
-    //    printf("%f %f %f\n", coo2[0], coo2[1], coo2[2]);
-    //}
+    float coo1[3], coo2[3];
+    int f=0, nf=1;  // current frame and total frames
+    int nf3 = nf*3; // Precalculate 3 * nframes for the coordinate lookup macro
 
     // Iterate over the two group sets
     for (g1=0; g1 < gn1; g1++){
@@ -22,17 +31,19 @@ void mindist_ext(float* coords, int* groups1, int* groups2, int gn1, int gn2, in
             for (a = 0; a < na; a++) {
                 g1atm = groups1[g1 * na + a];
                 if (g1atm == -1) break;
-                const float* coo1 = &coords[g1atm*dim];
+                //get_coords(coords, g1atm, f, nf, coo1);
+                //const float* coo1 = &coords[g1atm*dim];
 
                 for (b=0; b < na; b++) {
                     g2atm = groups2[g2 * na + b];
                     if (g2atm == -1) break;
-                    const float* coo2 = &coords[g2atm*dim];
+                    //get_coords(coords, g2atm, f, nf, coo2);
+                    //const float* coo2 = &coords[g2atm*dim];
 
                     float d[3];
-                    d[0] = coo1[0]-coo2[0];
-                    d[1] = coo1[1]-coo2[1];
-                    d[2] = coo1[2]-coo2[2];
+                    d[0] = coords[X(g1atm,f,nf,nf3)]-coords[X(g2atm,f,nf,nf3)];
+                    d[1] = coords[Y(g1atm,f,nf,nf3)]-coords[Y(g2atm,f,nf,nf3)];
+                    d[2] = coords[Z(g1atm,f,nf,nf3)]-coords[Z(g2atm,f,nf,nf3)];
                     //printf("coor: %f %f %f / %f %f %f\n", coo1[0], coo1[1], coo1[2], coo2[0], coo2[1], coo2[2]);
 
                     float D = d[0]*d[0]+d[1]*d[1]+d[2]*d[2];
