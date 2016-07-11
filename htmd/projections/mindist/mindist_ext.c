701189a9e43882a0cbd8d45ca20261b5d8eb33d3
Stefan Doerr <stefan.doerr@upf.edu>
2016-07-11 16:51:53 +0200

    min distance done entirely in C. removed deprecated projections

--- htmd/projections/mindist/mindist_ext.c
+++ htmd/projections/mindist/mindist_ext.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define X(atom, frame, nframes, nf3) atom*nf3+frame
-#define Y(atom, frame, nframes, nf3) atom*nf3+nframes+frame
-#define Z(atom, frame, nframes, nf3) atom*nf3+2*nframes+frame
+#define X(atom, frame, nframes, nf3) atom*nf3
+#define Y(atom, frame, nframes, nf3) atom*nf3+1
+#define Z(atom, frame, nframes, nf3) atom*nf3+2
+
+// I redefine here for frames to optimize calculations in the single frame use case
+#define Xf(atom, frame, nframes, nf3) atom*nf3+frame
+#define Yf(atom, frame, nframes, nf3) atom*nf3+nframes+frame
+#define Zf(atom, frame, nframes, nf3) atom*nf3+2*nframes+frame
 
 
 const int dim = 3;
@@ -16,7 +21,7 @@ const int dim = 3;
 //    c[2] = coords[atom*3*nframes+2*nframes+frame];
 //}
 
-void mindist_ext(float* coords, int* groups1, int* groups2, int gn1, int gn2, int na, float* dist) {
+void mindist_single_frame(float* coords, int* groups1, int* groups2, int gn1, int gn2, int na, float* dist) {
     float mindist;
     int a,b,g1,g2,n1,n2,g1atm,g2atm;
     float coo1[3], coo2[3];
@@ -56,6 +61,54 @@ void mindist_ext(float* coords, int* groups1, int* groups2, int gn1, int gn2, in
             }
             //printf("%d %d %d %d: %f\n", g1, gn2, g2, g1*gn2+g2, sqrt(mindist));
             dist[g1*gn2+g2] = sqrt(mindist);
+        }
+    }
+}
+
+
+void mindist_trajectory(float* coords, int* groups1, int* groups2, int gn1, int gn2, int na, int nf, float* dist) {
+    float mindist;
+    int a,b,g1,g2,n1,n2,g1atm,g2atm,f;
+    float coo1[3], coo2[3];
+    int nf3 = nf*3; // Precalculate 3 * nframes for the coordinate lookup macro
+    int groupprod = gn1*gn2;
+
+    // Iterate over all frames
+    for (f=0; f < nf; f++){
+        // Iterate over the two group sets
+        for (g1=0; g1 < gn1; g1++){
+            for (g2=0; g2 < gn2; g2++){
+                mindist = -1;
+                // Iterate over atoms in the two groups
+                for (a = 0; a < na; a++) {
+                    g1atm = groups1[g1 * na + a];
+                    if (g1atm == -1) break;
+                    //get_coords(coords, g1atm, f, nf, coo1);
+                    //const float* coo1 = &coords[g1atm*dim];
+
+                    for (b=0; b < na; b++) {
+                        g2atm = groups2[g2 * na + b];
+                        if (g2atm == -1) break;
+                        //get_coords(coords, g2atm, f, nf, coo2);
+                        //const float* coo2 = &coords[g2atm*dim];
+
+                        float d[3];
+                        d[0] = coords[Xf(g1atm,f,nf,nf3)]-coords[Xf(g2atm,f,nf,nf3)];
+                        d[1] = coords[Yf(g1atm,f,nf,nf3)]-coords[Yf(g2atm,f,nf,nf3)];
+                        d[2] = coords[Zf(g1atm,f,nf,nf3)]-coords[Zf(g2atm,f,nf,nf3)];
+                        //printf("coor: %f %f %f / %f %f %f\n", coo1[0], coo1[1], coo1[2], coo2[0], coo2[1], coo2[2]);
+
+                        float D = d[0]*d[0]+d[1]*d[1]+d[2]*d[2];
+                        //printf("index: %d/%d dist: %f\n", g1atm, g2atm, sqrt(D));
+
+                        if (D < mindist || mindist < 0) {
+                            mindist = D;
+                        }
+                    }
+                }
+                //printf("%d %d %d %d: %f\n", g1, gn2, g2, g1*gn2+g2, sqrt(mindist));
+                dist[g1*gn2+g2+(f*groupprod)] = sqrt(mindist);  // Add here the f
+            }
         }
     }
 }
