bb11dffb968f0bfbcaacdbed5e59a388769ad3f3
Stefan Doerr <stefan.doerr@upf.edu>
2016-07-12 14:25:18 +0200

    fixed argument passing to build script

--- C/distance/build.sh
+++ C/distance/build.sh
@@ -4,20 +4,20 @@ make clean
 
 for TYPE in pro basic; do
 #for TRAVIS_OS_NAME in "linux" "windows"; do #  "windows" "osx"; do
-	if [ "$TRAVIS_OS_NAME" == "linux"   ]; then
+	if [ "$1" == "linux"   ]; then
 		PLATFORM=Linux; 
 		CC=gcc
 		CXX=g++	
-	elif [ "$TRAVIS_OS_NAME" == "windows" ]; then 
+	elif [ "$1" == "windows" ]; then 
 		PLATFORM=Windows;
 		CC=x86_64-w64-mingw32-gcc
 		CXX=x86_64-w64-mingw32-g++
-	elif [ "$TRAVIS_OS_NAME" == "osx"     ]; then 
+	elif [ "$1" == "osx"     ]; then 
 		PLATFORM=Darwin; 
 		CC=gcc
 		CXX=g++
 	else
-		echo "Unknown OS [$TRAVIS_OS_NAME]"
+		echo "Unknown OS [$1]"
 		exit 1
 	fi
 	make all CC=$CC CXX=$CXX PLATFORM=$PLATFORM  TYPE=$TYPE
