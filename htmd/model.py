corrected the calculation of the equilibrium distribution

htmd/model.py
@@ -308,7 +308,8 @@ class Model(object):
         self._integrityCheck(postmsm=True)
         macroeq = np.ones(self.macronum) * -1
         for i in range(self.macronum):
-            macroeq[i] = np.sum(self.msm.stationary_distribution[self.macro_ofmicro == i])
+            # macroeq[i] = np.sum(self.msm.stationary_distribution[self.macro_ofmicro == i])
+            macroeq[i] = np.sum(self.msm.metastable_memberships[:, i] * self.msm.stationary_distribution)
 
         if plot:
             from matplotlib import pylab as plt