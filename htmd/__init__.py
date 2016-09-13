7c9ec470c6737bbffe781cf1b92df7492bfa4de6
Stefan Doerr <stefan.doerr@upf.edu>
2016-09-13 17:23:16 +0200

    fixed imports to moved ACEMD class

--- htmd/__init__.py
+++ htmd/__init__.py
@@ -26,7 +26,8 @@ from htmd.model import Model, getStateStatistic
 from htmd.kinetics import Kinetics
 from htmd.vmdviewer import viewer, getCurrentViewer
 from htmd.builder.solvate import solvate
-from htmd.acemd.acemd import Acemd
+from htmd.apps.acemd import Acemd
+from htmd.apps.pmemd import Pmemd
 from htmd.apps.acemdlocal import AcemdLocal
 from htmd.apps.lsf import LSF
 from htmd.apps.aws import AWS
