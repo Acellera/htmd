c499458f62d8b2658336647b908a5f10976c5ae4
j3mdamas <jmdamas@gmail.com>
2016-10-15 16:07:23 +0200

    new, revised documentation/docstrings, spacing corrections

--- htmd/protocols/__init__.py
+++ htmd/protocols/__init__.py
@@ -1,9 +1,7 @@
 """
-Protocols
-=========
-
-HTMD provides a variety of protocols for setting up simulations.
-The most basic include equilibration and production runs with support for flat-bottom potentials and constraints for atoms.
+HTMD provides a variety of protocols for setting up different types of simulations.
+The most basic protocols include equilibration and production runs, both of which support flat-bottom potentials and
+constraints for atoms.
 """
 
 # (c) 2015-2016 Acellera Ltd http://www.acellera.com
