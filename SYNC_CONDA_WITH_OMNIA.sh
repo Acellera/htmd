9885e8e15a620e03fa2b1d8440f5cef3f8b8a50a
M J Harvey <moonbug1970@gmail.com>
2016-11-01 10:58:15 +0000

    add script for syncing acellera channel with omnia's

---
+++ SYNC_CONDA_WITH_OMNIA.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+anaconda copy --to-owner acellera --to-label omnia omnia/ambermini/15.0.4
+anaconda copy --to-owner acellera --to-label omnia omnia/bhmm/0.6.1
+anaconda copy --to-owner acellera --to-label omnia omnia/funcsigs/1.0.0
+anaconda copy --to-owner acellera --to-label omnia omnia/mdtraj/1.7.2
+anaconda copy --to-owner acellera --to-label omnia omnia/msmtools/1.1.4
+anaconda copy --to-owner acellera --to-label omnia omnia/openbabel/2015.09
+anaconda copy --to-owner acellera --to-label omnia omnia/pint/0.7.2
+anaconda copy --to-owner acellera --to-label omnia omnia/progress_reporter/1.2
+anaconda copy --to-owner acellera --to-label omnia omnia/pyemma/2.2.6
+anaconda copy --to-owner acellera --to-label omnia omnia/thermotools/0.2.3
+
