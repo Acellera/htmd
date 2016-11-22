77b2ff3e0c4229b249d0bc6de12b4830059f7717
Stefan Doerr <stefan.doerr@upf.edu>
2016-11-22 15:09:41 +0100

    added tests for the Metric.set method

--- htmd/projections/metric.py
+++ htmd/projections/metric.py
@@ -281,3 +281,26 @@ def _projectionGenerator(metric, ncpus):
 def _projector(metric, i):
     return metric._projectSingle(i)
 
+if __name__ == '__main__':
+    from htmd.molecule.molecule import Molecule
+    from htmd.projections.metricrmsd import MetricRmsd
+
+    # Testing the set method of Metric
+    sims = []
+    ref = Molecule('3PTB')
+    metr = Metric(sims)
+    metr.set(MetricRmsd(ref, 'protein and name CA'))
+    assert len(metr.projectionlist) == 1
+
+    metr.set([MetricRmsd(ref, 'protein and name CA'), MetricRmsd(ref, 'protein and name CA')])
+    assert len(metr.projectionlist) == 2
+
+    def foo(mol, ref):
+        from htmd.molecule.util import molRMSD
+        mol.wrap('protein')
+        mol.align('protein and name CA', refmol=ref)
+        return molRMSD(mol, ref, mol.atomselect('protein and name CA'), ref.atomselect('protein and name CA'))
+
+    metr = Metric(sims)
+    metr.set((foo, (ref,)))
+    assert len(metr.projectionlist) == 1
