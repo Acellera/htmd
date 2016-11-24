8c962c6398aacfafdfb3beec8f6fdca5d61a01c7
M J Harvey <moonbug1970@gmail.com>
2016-11-24 17:46:54 +0000

    catch later version of glibc

--- tests/other_tests/check_glibc/run.sh
+++ tests/other_tests/check_glibc/run.sh
@@ -2,7 +2,7 @@
 
 
 
-strings ../../../htmd/lib/*/Linux/*so ../../../htmd/lib/Linux/*so | grep -q GLIBC_2.14
+strings ../../../htmd/lib/*/Linux/*so ../../../htmd/lib/Linux/*so | egrep -q -e  'GLIBC_2.1[45]'
 if [ "$?" == "0" ]; then
 	echo "GLIBC 2.14 use detected in Linux dsos"
 	exit 1
