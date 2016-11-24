12171fd49de5f5b3f181e44d49a049b5ba2d5b39
Stefan Doerr <stefan.doerr@upf.edu>
2016-11-24 12:22:59 +0100

    added MetricTMscore to init

--- htmd/__init__.py
+++ htmd/__init__.py
@@ -15,6 +15,7 @@ from htmd.projections.metricdihedral import MetricDihedral
 from htmd.projections.metricshell import MetricShell
 from htmd.projections.metricsecondarystructure import MetricSecondaryStructure
 from htmd.projections.metricsasa import MetricSasa
+from htmd.projections.metrictmscore import MetricTMscore
 from htmd.projections.metric import Metric
 from htmd.projections.tica import TICA
 from htmd.projections.kmeanstri import KMeansTri
