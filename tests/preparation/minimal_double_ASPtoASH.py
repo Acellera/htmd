6afe714b4c8b7eb801ff54844dd60d64087d3327
Toni G <toni.giorgino@gmail.com>
2016-12-12 18:50:21 +0100

    debugging

---
+++ tests/preparation/minimal_double_ASPtoASH.py
@@ -0,0 +1,29 @@
+# Gerard's test case
+
+from htmd import *
+import pickle
+import sys
+sys.setrecursionlimit(10000)
+
+#Input and 1st visualization
+mol = Molecule('4uai')
+mol.filter("protein and chain A")
+prep, prepdata = proteinPrepare(mol, pH=3, returnDetails=True)
+d = prepdata.data
+
+toChange = (d.resid == int(52)) & (d.chain == str("A"))
+
+print(d.values[51])
+d[toChange]
+d.loc[toChange, "forced_protonation"] = "ASH"
+# or, faster -- d.set_value(toChange,"forced_protonation","ASH")
+
+#I do a reprepare before the proper reprepare
+reprep, reprepdata = prepdata.reprepare()
+u = reprepdata.data
+print(u.values[51])
+
+reprep, reprepdata = prepdata.reprepare()
+u = reprepdata.data
+print(u.values[51])
+
