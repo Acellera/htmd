b86664b274d21dff41d68a575bc7ac2e417ea7ca
Stefan Doerr <stefan.doerr@upf.edu>
2017-01-25 15:01:10 +0100

    cleaned up the static pages mess. made sphinx menu identical to static menu

--- doc/render_jinja.py
+++ doc/render_jinja.py
@@ -10,15 +10,15 @@ outd = sys.argv[2]
 def render_html(indir, outdir):
     indir = os.path.abspath(indir)
     outdir = os.path.abspath(outdir)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
-    templatedir = os.path.join(indir, 'templates')
-
-    env = Environment(loader=FileSystemLoader(templatedir))
+    env = Environment(loader=FileSystemLoader(indir))
     for t in env.list_templates():
-        if t == 'common.html':
+        if t == 'common.html' or t == 'navbar.html':
             continue
         temp = env.get_template(t)
-        html = temp.render({'name': t})
+        html = temp.render()
         with open(os.path.join(outdir, t), 'w') as f:
             f.write(html)
 
