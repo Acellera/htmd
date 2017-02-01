85faf4503bae1f90ef46e96f4445289a546402af
j3mdamas <jmdamas@gmail.com>
2017-02-01 19:01:31 +0100

    [dev] change to ambermini 16.16.0 (hardcoded on dependencies); Defaults changed from leaprc.ff14SB to oldff/leaprc.ff14SB; changed the default system to match charmm.py; data for amber tests updated accordingly (Closes #236)

--- htmd/builder/amber.py
+++ htmd/builder/amber.py
@@ -45,15 +45,15 @@ def listFiles():
     for f in ffs:
         print(f)
 
-    amberdir = path.join(amberhome, 'dat', 'leap', 'cmd', 'oldff')
-    ffs = [f for f in os.listdir(amberdir) if path.isfile(path.join(amberdir, f))]
+    oldffdir = path.join(amberhome, 'dat', 'leap', 'cmd', 'oldff')
+    ffs = [path.join('oldff', f) for f in os.listdir(oldffdir) if path.isfile(path.join(oldffdir, f))]
     print('---- OLD Forcefield files list: ' + path.join(amberdir, '') + ' ----')
     for f in ffs:
         print(f)
 
     # FRCMOD files
     frcmoddir = path.join(amberhome, 'dat', 'leap', 'parm')
-    ffs = glob(frcmoddir+"/frcmod.*")
+    ffs = [f for f in os.listdir(frcmoddir) if path.isfile(path.join(frcmoddir, f)) and f.startswith('frcmod')]
     print('---- Parameter files list: ' + path.join(frcmoddir, '') + ' ----')
     for f in ffs:
         print(path.basename(f))
@@ -68,6 +68,20 @@ def listFiles():
         print(f)
 
 
+def defaultFf():
+    """ Returns the default leaprc forcefield files used by amber.build """
+    return ['leaprc.lipid14', path.join('oldff', 'leaprc.ff14SB'), 'leaprc.gaff']
+
+
+def defaultTopo():
+    """ Returns the default topology `prepi` files used by amber.build """
+    return []
+
+
+def defaultParam():
+    """ Returns the default parameter `frcmod` files used by amber.build """
+    return ['frcmod.ionsjc_tip3p']
+
 
 def build(mol, ff=None, topo=None, param=None, prefix='structure', outdir='./build', caps=None, ionize=True, saltconc=0,
           saltanion=None, saltcation=None, disulfide=None, tleap='tleap', execute=True, atomtypes=None,
@@ -81,11 +95,17 @@ def build(mol, ff=None, topo=None, param=None, prefix='structure', outdir='./bui
     mol : :class:`Molecule <htmd.molecule.molecule.Molecule>` object
         The Molecule object containing the system
     ff : list of str
-        A list of leaprc forcefield files. Default: ['leaprc.lipid14', 'leaprc.ff14SB', 'leaprc.gaff']
+        A list of leaprc forcefield files.
+        Use :func:`amber.listFiles <htmd.builder.amber.listFiles>` to get a list of available forcefield files.
+        Default: :func:`amber.defaultFf <htmd.builder.amber.defaultFf>`
     topo : list of str
         A list of topology `prepi` files.
+        Use :func:`amber.listFiles <htmd.builder.amber.listFiles>` to get a list of available topology files.
+        Default: :func:`amber.defaultTopo <htmd.builder.amber.defaultTopo>`
     param : list of str
-        A list of parameter `frcmod` files. Default: ['frcmod.ionsjc_tip3p',]
+        A list of parameter `frcmod` files.
+        Use :func:`amber.listFiles <htmd.builder.amber.listFiles>` to get a list of available parameter files.
+        Default: :func:`amber.defaultParam <htmd.builder.amber.defaultParam>`
     prefix : str
         The prefix for the generated pdb and psf files
     outdir : str
@@ -132,10 +152,8 @@ def build(mol, ff=None, topo=None, param=None, prefix='structure', outdir='./bui
     >>> molbuilt = amber.build(mol, outdir='/tmp/build')  # doctest: +SKIP
     ...
     >>> # More complex example
-    >>> ffs = ['leaprc.lipid14', 'leaprc.ff14SB', 'leaprc.gaff']
-    >>> params = ['frcmod.ionsjc_tip3p',]
     >>> disu = [DisulfideBridge('P', 157, 'P', 13), DisulfideBridge('K', 1, 'K', 25)]
-    >>> molbuilt = amber.build(mol, ff=ffs, param=params, outdir='/tmp/build', saltconc=0.15, disulfide=disu)  # doctest: +SKIP
+    >>> molbuilt = amber.build(mol, foutdir='/tmp/build', saltconc=0.15, disulfide=disu)  # doctest: +SKIP
     """
     # Remove pdb bonds!
     mol = mol.copy()
@@ -146,11 +164,11 @@ def build(mol, ff=None, topo=None, param=None, prefix='structure', outdir='./bui
         os.makedirs(outdir)
     _cleanOutDir(outdir)
     if ff is None:
-        ff = ['leaprc.lipid14', 'leaprc.ff14SB', 'leaprc.gaff']
+        ff = defaultFf()
     if topo is None:
-        topo = []
+        topo = defaultTopo()
     if param is None:
-        param = ['frcmod.ionsjc_tip3p',]
+        param = defaultParam()
     if caps is None:
         caps = _defaultProteinCaps(mol)
 
@@ -612,7 +630,7 @@ if __name__ == '__main__':
         mol = proteinPrepare(mol)
         mol.filter('protein')  # Fix for bad proteinPrepare hydrogen placing
         smol = solvate(mol)
-        ffs = ['leaprc.lipid14', 'leaprc.ff14SB', 'leaprc.gaff']
+        ffs = defaultFf()
         tmpdir = tempname()
         bmol = build(smol, ff=ffs, outdir=tmpdir)
 
@@ -628,7 +646,7 @@ if __name__ == '__main__':
         mol = Molecule(pid)
         mol.filter('protein')
         smol = solvate(mol)
-        ffs = ['leaprc.lipid14', 'leaprc.ff14SB', 'leaprc.gaff']
+        ffs = defaultFf()
         tmpdir = tempname()
         bmol = build(smol, ff=ffs, outdir=tmpdir)
 
