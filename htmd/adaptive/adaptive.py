6040ff767b266c56b3526783f050e26cb39db7fe
Stefan Doerr <stefan.doerr@upf.edu>
2017-02-08 16:26:32 +0100

    added utility functions for getting epoch number from simulation name and simulation indexes for each epoch

--- htmd/adaptive/adaptive.py
+++ htmd/adaptive/adaptive.py
@@ -425,6 +425,51 @@ class Adaptive(object):
         return
 
 
+def epochSimIndexes(simlist):
+    """ Finds the simulation indexes for each epoch.
+
+    Creates a dictionary with the epoch number as key and values the simlist indexes of the simulations corresponding to
+    the given epoch.
+
+    Parameters
+    ----------
+    simlist : list
+        A simulation list created using the :func:`simlist <htmd.simlist.simlist>` function
+
+    Returns
+    -------
+
+    """
+    epochidx = {}
+    for i, sl in enumerate(simlist):
+        simepoch = getEpochFromName(sl.trajectory[0])
+        if simepoch not in epochidx:
+            epochidx[simepoch] = []
+        epochidx[simepoch].append(i)
+    return epochidx
+
+
+def getEpochFromName(name):
+    """ Given a adaptive simulation name, tells you which epoch it belongs to.
+
+    Parameters
+    ----------
+    name : str
+        Simulation name
+
+    Returns
+    -------
+    epoch : int
+        The epoch
+    """
+    import re
+    reg = re.compile('/e(\d+)s\d+_')
+    matches = reg.findall(name)
+    if len(matches) == 0:
+        raise RuntimeError('{} is not an adaptive trajectory'.format(name))
+    return int(matches[0])
+
+
 def reconstructAdaptiveTraj(simlist, trajID):
     """ Reconstructs a long trajectory out of short adaptive runs.
 
