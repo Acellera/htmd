f6f6a7bd5158b92b07697f1a1af71d9325f2efc9
Stefan Doerr <stefan.doerr@upf.edu>
2017-06-26 12:17:58 +0200

    added metricsphericalcoordinate

--- htmd/projections/metricsphericalcoordinate.py
+++ htmd/projections/metricsphericalcoordinate.py
@@ -9,15 +9,18 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class MetricCoordinate(Projection):
-    """ Creates a MetricCoordinate object that calculates the atom coordinates from a set of trajectories.
+class MetricSphericalCoordinate(Projection):
+    """ Creates a MetricSphericalCoordinate object that calculates the spherical coordinates between two centers of
+    masses from a set of trajectories.
 
     Parameters
     ----------
     refmol : :class:`Molecule <htmd.molecule.molecule.Molecule>` object
         The reference Molecule to which we will align.
-    atomsel : str
-        Atomselection for the atoms whose coordinates we want to retrieve.
+    targetcom : str
+        The atom selection from which to calculate the target center of mass.
+    refcom : str
+        The atom selection from which to calculate the reference center of mass.
     trajalnstr : str, optional
         Atomselection for the trajectories from which to align to the reference structure.
     refalnstr : str, optional
@@ -31,14 +34,15 @@ class MetricCoordinate(Projection):
     -------
     metr : MetricCoordinate object
     """
-    def __init__(self, refmol, atomsel, trajalnstr='protein and name CA', refalnstr=None, centerstr='protein', pbc=True):
+    def __init__(self, refmol, targetcom, refcom, trajalnstr='protein and name CA', refalnstr=None, centerstr='protein', pbc=True):
         if refalnstr is None:
             refalnstr = trajalnstr
         self._refmol = refmol
         self._refalnsel = self._refmol.atomselect(refalnstr)
         self._trajalnsel = trajalnstr
         self._centersel = centerstr
-        self._atomsel = atomsel
+        self._targetcom = targetcom
+        self._refcom = refcom
         self._pc_trajalnsel = None  # pc = Pre-calculated
         self._pc_atomsel = None
         self._pc_centersel = None
@@ -46,7 +50,8 @@ class MetricCoordinate(Projection):
 
     def _precalculate(self, mol):
         self._pc_trajalnsel = mol.atomselect(self._trajalnsel)
-        self._pc_atomsel = mol.atomselect(self._atomsel)
+        self._pc_targetcom = mol.atomselect(self._targetcom)
+        self._pc_refcom = mol.atomselect(self._refcom)
         self._pc_centersel = mol.atomselect(self._centersel)
 
     def project(self, mol):
@@ -62,32 +67,44 @@ class MetricCoordinate(Projection):
         data : np.ndarray
             An array containing the projected data.
         """
-        (trajalnsel, atomsel, xxx) = self._getSelections(mol)
+        trajalnsel, targetcom, refcom, centersel = self._getSelections(mol)
 
         mol = mol.copy()
         if self.pbc:
             mol.wrap(self._centersel)
         mol.align(trajalnsel, refmol=self._refmol, refsel=self._refalnsel)
 
-        coords = np.concatenate((mol.coords[atomsel, 0, :], mol.coords[atomsel, 1, :], mol.coords[atomsel, 2, :]))
-        return np.transpose(coords)  # I need to permute the dimensions here to put frames on the first
+        refcom = np.mean(mol.coords[refcom, :, :], axis=0)
+        targetcom = np.mean(mol.coords[targetcom, :, :], axis=0)
+        xyz = targetcom - refcom
+
+        r = np.sqrt(np.sum(xyz ** 2, axis=0))
+        theta = np.arccos(xyz[2, :] / r)
+        phi = np.arctan(xyz[1, :] / xyz[0, :])
+
+        return np.stack((r, theta, phi), axis=1)
 
     def _getSelections(self, mol):
-        if self._pc_trajalnsel is not None and self._pc_atomsel is not None and self._pc_centersel is not None:
+        if self._pc_trajalnsel is not None and self._pc_refcom is not None and self._pc_targetcom is not None \
+                and self._pc_centersel is not None:
             trajalnsel = self._pc_trajalnsel
-            atomsel = self._pc_atomsel
+            targetcom = self._pc_targetcom
+            refcom = self._pc_refcom
             centersel = self._pc_centersel
         else:
             trajalnsel = mol.atomselect(self._trajalnsel)
-            atomsel = mol.atomselect(self._atomsel)
+            targetcom = mol.atomselect(self._targetcom)
+            refcom = mol.atomselect(self._refcom)
             centersel = mol.atomselect(self._centersel)
         if np.sum(trajalnsel) == 0:
             raise NameError('Alignment selection resulted in 0 atoms.')
-        if np.sum(atomsel) == 0:
-            raise NameError('Atom selection resulted in 0 atoms.')
+        if np.sum(targetcom) == 0:
+            raise NameError('Atom selection for `targetcom` resulted in 0 atoms.')
+        if np.sum(refcom) == 0:
+            raise NameError('Atom selection for `refcom` resulted in 0 atoms.')
         if np.sum(centersel) == 0:
             raise NameError('Centering selection resulted in 0 atoms.')
-        return trajalnsel, atomsel, centersel
+        return trajalnsel, targetcom, refcom, centersel
 
     def getMapping(self, mol):
         """ Returns the description of each projected dimension.
@@ -102,17 +119,13 @@ class MetricCoordinate(Projection):
         map : :class:`DataFrame <pandas.core.frame.DataFrame>` object
             A DataFrame containing the descriptions of each dimension
         """
-        (xxx, atomsel, yyy) = self._getSelections(mol)
-        atomidx = np.where(atomsel)[0]
+        trajalnsel, targetcom, refcom, centersel = self._getSelections(mol)
+        targetatomidx = np.where(targetcom)[0]
+        refatomidx = np.where(refcom)[0]
         from pandas import DataFrame
-        types = []
-        indexes = []
-        description = []
-        for xyz in ('X', 'Y', 'Z'):
-            for i in atomidx:
-                types += ['coordinate']
-                indexes += [i]
-                description += ['{} coordinate of {} {} {}'.format(xyz, mol.resname[i], mol.resid[i], mol.name[i])]
+        types = ['r', 'theta', 'phi']
+        indexes = [[targetatomidx, refatomidx]] * 3
+        description = ['r', 'theta', 'phi']
         return DataFrame({'type': types, 'atomIndexes': indexes, 'description': description})
 
 
@@ -120,16 +133,11 @@ if __name__ == "__main__":
     from htmd.molecule.molecule import Molecule
     from htmd.home import home
     from os import path
-    mol = Molecule(path.join(home(), 'data', 'metricdistance', 'filtered.pdb'))
+    ref = Molecule(path.join(home(dataDir='metricdistance'), 'filtered.pdb'))
+    mol = ref.copy()
     mol.read(path.join(home(), 'data', 'metricdistance', 'traj.xtc'))
-    ref = mol.copy()
-    ref.coords = np.atleast_3d(ref.coords[:, :, 0])
-    metr = MetricCoordinate(ref, 'protein and name CA')
-    data = metr.project(mol)
 
-    lastcoors = np.array([6.79283285,   5.55226946,   4.49387407,   2.94484425,
-                          5.36937141,   3.18590879,   5.75874281,   5.48864174,
-                          1.69625032,   1.58790839,   0.57877392,  -2.66498065,
-                          -3.70919156,  -3.33702421,  -5.38465405,  -8.43286991,
-                          -8.15859032,  -7.85062265, -10.92551327, -13.70733166], dtype=np.float32)
-    assert np.all(np.abs(data[-1, -20:] - lastcoors) < 0.001), 'Coordinates calculation is broken'
+    res = MetricSphericalCoordinate(ref, 'resname MOL', 'within 8 of resid 98').project(mol)
+    _ = MetricSphericalCoordinate(ref, 'resname MOL', 'within 8 of resid 98').getMapping(mol)
+
+    assert np.allclose(res, np.load(path.join(home(dataDir='test-metrics'), 'metricsphericalcoordinate', 'res.npy')))
