91562f4644c5411989c864ceab90dfe3266204ae
Stefan Doerr <stefan.doerr@upf.edu>
2017-08-08 15:38:23 +0200

    trying to disable htmdlib building in PRs

---
+++ continuous-integration/appveyor/compile_htmdlib.sh
@@ -0,0 +1,12 @@
+cd ..
+git clone https://%GITHUB_HTMDLIB_USERNAME%:%GITHUB_HTMDLIB_PASSWORD%@github.com/Acellera/htmdlib --depth 1
+choco install mingw
+refreshenv
+"SET PATH=%PYTHON%;%PYTHON%\\Scripts;%PATH%"  # refreshenv ruins my previous python path
+conda create -q -n py27 python=2.7
+activate py27
+conda install -c anaconda scons
+cd htmdlib/C
+scons --prefix=../../htmd/htmd/lib/Windows/
+cd ../../htmd/
+deactivate py27
