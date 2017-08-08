c41746649e1f4e644e5ddc8742a8965a4812fe45
Stefan Doerr <stefan.doerr@upf.edu>
2017-08-08 16:11:06 +0200

    added error for writing filetypes which are not supported

--- htmd/molecule/molecule.py
+++ htmd/molecule/molecule.py
@@ -1197,6 +1197,9 @@ class Molecule:
             ext = type
         if ext in _WRITERS:
             _WRITERS[ext](src, filename)
+        else:
+            raise IOError('Molecule cannot write files with "{}" extension yet. If you need such support please notify '
+                          'us on the github htmd issue tracker.'.format(ext))
 
     def empty(self, numAtoms):
         """ Creates an empty molecule of N atoms.
