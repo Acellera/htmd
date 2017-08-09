05d0846cbe26d656d9e4e0b0c98b825ad86cbaa7
Raimondas Galvelis <r.galvelis@acellera.com>
2017-08-09 10:20:47 +0200

    Parameterize: reorganize tests

--- htmd/parameterization/test.py
+++ htmd/parameterization/test.py
@@ -11,60 +11,85 @@ from tempfile import TemporaryDirectory
 from subprocess import call
 from filecmp import cmp
 
+os.environ['HTMD_NONINTERACTIVE'] = '1'
 
 class TestParametrize(unittest.TestCase):
 
-    def tests(self):
+    def setUp(self):
 
-        testDir = os.path.join('..', 'data', 'test-param')
-        if not os.path.exists(testDir):
-            testDir = os.path.join('htmd', 'data', 'test-param')
-        testDir = os.path.abspath(testDir)
+        self.dataDir = os.path.join('..', 'data', 'test-param')
+        if not os.path.exists(self.dataDir):
+            self.dataDir = os.path.join('htmd', 'data', 'test-param')
+        self.dataDir = os.path.abspath(self.dataDir)
 
-        refDirs = [os.path.join(testDir, test) for test in os.listdir(testDir)]
-        refDirs = [test for test in refDirs if os.path.isdir(test)]
+    def test_doc(self):
 
-        for refDir in refDirs:
-            with self.subTest(refDir=refDir), TemporaryDirectory() as resDir:
-                shutil.copy(os.path.join(refDir, 'stdin'), resDir)
-                molFile = os.path.join(refDir, 'input.mol2')
-                if os.path.exists(molFile):
-                    shutil.copy(molFile, resDir)
+        self._test(os.path.join(self.dataDir, 'doc'))
 
-                with open(os.path.join(resDir, 'stdin')) as stdin, \
-                     open(os.path.join(resDir, 'stdout'), 'w') as stdout, \
-                     open(os.path.join(resDir, 'stderr'), 'w') as stderr:
-                    arguments = stdin.readline().split()
-                    returncode = call(arguments, stdout=stdout, stderr=stderr, cwd=resDir)
-                    self.assertEqual(returncode, 0)
+    def test_h2o2_list(self):
 
-                for directory, _, files in os.walk(refDir):
-                    for file in files:
-                        file = os.path.relpath(os.path.join(directory, file), refDir)
-                        refFile, resFile = os.path.join(refDir, file), os.path.join(resDir, file)
+        self._test(os.path.join(self.dataDir, 'list'))
+
+    def test_h2o2_gaff2(self):
+
+        self._test(os.path.join(self.dataDir, 'gaff2'))
+
+    def _test(self, refDir):
+
+        with TemporaryDirectory() as resDir:
+            shutil.copy(os.path.join(refDir, 'stdin'), resDir)
+            molFile = os.path.join(refDir, 'input.mol2')
+            if os.path.exists(molFile):
+                shutil.copy(molFile, resDir)
+
+            with open(os.path.join(resDir, 'stdin')) as stdin, \
+                 open(os.path.join(resDir, 'stdout'), 'w') as stdout, \
+                 open(os.path.join(resDir, 'stderr'), 'w') as stderr:
+                arguments = stdin.readline().split()
+                returncode = call(arguments, stdout=stdout, stderr=stderr, cwd=resDir)
+                self.assertEqual(returncode, 0)
+
+            for directory, _, files in os.walk(refDir):
+                for file in files:
+                    file = os.path.relpath(os.path.join(directory, file), refDir)
+
+                    with self.subTest(file=file):
+                        refFile = os.path.join(refDir, file)
+                        resFile = os.path.join(resDir, file)
 
                         if file in ('stdout'):
-                            with open(refFile) as ref, open(resFile) as res:
-                                refLines, resLines = ref.readlines(), res.readlines()
+                            with open(refFile) as ref:
+                                refLines = []
+                                for line in ref.readlines():
+                                    if re.search('HTMD: Logging setup failed', line):
+                                        continue
+                                    if re.search('Number of CPUs to use \(default:', line):
+                                        continue
+                                    if re.search('ncpus:', line):
+                                        continue
+                                    refLines.append(line)
 
-                                refLines = [line for line in refLines if not re.search('Deprecation warning:', line)]
-                                refLines = [line for line in refLines if not re.search('To import all HTMD shortcuts please from now on use', line)]
+                            with open(resFile) as res:
+                                resLines = []
+                                for line in res.readlines():
+                                    if re.search('HTMD: Logging setup failed', line):
+                                        continue
+                                    if re.search('Number of CPUs to use \(default:', line):
+                                        continue
+                                    if re.search('ncpus:', line):
+                                        continue
+                                    resLines.append(line)
 
-                                resLines = [line for line in resLines if not re.search('Deprecation warning:', line)]
-                                resLines = [line for line in resLines if not re.search('To import all HTMD shortcuts please from now on use', line)]
-
-                            for refLine, resLine in zip(refLines, resLines):
-                                if re.search('HTMD version', refLine):
-                                    continue
-                                self.assertEqual(refLine, resLine, msg=refFile)
+                            self.assertListEqual(refLines, resLines, msg=file)
 
                         elif file.endswith('frcmod'):
                             with open(refFile) as ref, open(resFile) as res:
                                 # HACK! FRCMOD file lines are swapped randomly, so first sort them and compare.
                                 #       Also the first live with the version is removed
-                                refLines, resLines = sorted(ref.readlines()[1:]), sorted(res.readlines()[1:])
+                                refLines = sorted(ref.readlines()[1:])
+                                resLines = sorted(res.readlines()[1:])
 
-                            self.assertEqual(refLines, resLines, msg=refFile)
+                            self.assertListEqual(refLines, resLines, msg=file)
 
                         else:
                             self.assertTrue(cmp(refFile, resFile, shallow=False), msg=file)
