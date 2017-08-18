85ef90f11910453e7c9d717b37bc14e1c2cf6079
cuzzo87 <cuzzolin.alberto@gmail.com>
2017-08-18 16:06:33 +0200

    adds a new metric based on COM of two selections

--- htmd/projections/metriccmdistance.py
+++ htmd/projections/metriccmdistance.py
@@ -32,8 +32,7 @@ class MetricCmDistance(Projection):
         self.sel2 = sel2
         self.precalcsel1 = None
         self.precalcsel2 = None
-
-
+        
     def _precalculate(self, mol):
         self.precalcsel1 = self._processSelection(mol, self.sel1, None)
         self.precalcsel2 = self._processSelection(mol, self.sel2, None)
@@ -52,21 +51,61 @@ class MetricCmDistance(Projection):
             An array containing the projected data.
         """
 
-        logging.warning('No mapping feature available at the moment')
-
         (sel1, sel2) = self._getSelections(mol)
 
-        cm_sel1 = self._getMolCenter(mol.get('coords', sel1))
-        cm_sel2 = self._getMolCenter(mol.get('coords', sel2))
-
+        sel1_coords = []
+        sel2_coords = []
+        for f in range(mol.numFrames):
+            mol.frame=f
+            sel1_coords.append(mol.get('coords', sel1))
+            sel2_coords.append(mol.get('coords', sel2))
         
-        cm_dist = np.linalg.norm(cm_sel1 - cm_sel2)
 
-        metric = np.atleast_1d(cm_dist)
+        cms_sel1 =[ self._getMolCenter(coords) 
+                    for coords in sel1_coords ]
+        cms_sel2 =[ self._getMolCenter( coords) 
+                    for coords in sel2_coords ]
+
+
+        cms_dist =[ np.linalg.norm(cm_s1 - cm_s2) 
+                    for cm_s1, cm_s2 in zip(cms_sel1, cms_sel2) ]
+        
+
+        metric = np.atleast_1d( cms_dist)
+        
+
         return metric
 
     def _getMolCenter(self, coords):
         return np.mean(coords, axis=-2)
+
+    def getMapping(self, mol):
+        """ Returns the description of each projected dimension.
+
+        Parameters
+        ----------
+        mol : :class:`Molecule <htmd.molecule.molecule.Molecule>` object
+            A Molecule object which will be used to calculate the descriptions of the projected dimensions.
+
+        Returns
+        -------
+        map : :class:`DataFrame <pandas.core.frame.DataFrame>` object
+            A DataFrame containing the descriptions of each dimension
+        """
+        (sel1, sel2) = self._getSelections(mol)
+
+        sel1_definition = np.unique(mol.get('resid', sel1))
+        sel2_definition = np.unique(mol.get('resid', sel2))
+
+
+        from pandas import DataFrame
+        types = ['cmDist']
+        indexes = [ [ np.where(sel1)[0], np.where(sel2)[0] ] ]
+        description = ['Mass center distances between resids {} and resids {}'.format(sel1_definition, sel2_definition)]
+
+        return DataFrame({'type': types, 'atomIndexes': indexes, 'description': description})
+
+
 
     def _getSelections(self, mol):
         # If they have been pre-calculated return them.
