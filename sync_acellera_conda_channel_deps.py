e1936c9af82735cdfcdcacc1611ec1ca78704fb7
j3mdamas <jmdamas@gmail.com>
2017-08-31 07:06:21 +0200

    First attempt at fixing dependencies issue. * Removed some unnecessary dependencies * Changed dependencies version constraints to greater or equal * Changed the sync to occur inside Travis, and only to sync the particular version and build number

--- sync_acellera_conda_channel_deps.py
+++ sync_acellera_conda_channel_deps.py
@@ -6,9 +6,25 @@
 #
 
 import os
-from subprocess import call
+import sys
+from subprocess import call, check_output
+import json
 
 from binstar_client.utils import get_server_api
+
+if 'TRAVIS' not in os.environ:
+    print('This script should be run on Travis, with a fresh conda installation')
+    sys.exit(1)
+
+if os.getenv('TRAVIS_PYTHON_VERSION') == '3.6':
+    python_version = 'py36'
+elif os.getenv('TRAVIS_PYTHON_VERSION') == '3.5':
+    python_version = 'py35'
+else:
+    print('Python version {} not supported'.format(os.getenv('TRAVIS_PYTHON_VERSION')))
+    sys.exit(1)
+
+osname = os.getenv('TRAVIS_OS_NAME')
 
 api = get_server_api()
 
@@ -16,45 +32,49 @@ os.chdir('/tmp')
 
 # Add packages to sync in this list here
 
-packages = [
-    ['omnia', 'fftw3f'],
-    ['omnia', 'openmm'],
-    ['omnia', 'ambermini'],
-    ['omnia', 'bhmm'],
-    ['omnia', 'funcsigs'],
-    ['omnia', 'mdtraj'],
-    ['conda-forge', 'msmtools'],
-    ['openbabel', 'openbabel'],
-    ['omnia', 'pint'],
-    ['conda-forge', 'progress_reporter'],
-    ['conda-forge', 'pyemma'],
-    ['conda-forge', 'thermotools'],
-    ['bioconda', 'nglview'],
-    ['omnia', 'nglview'],  # Adding both omnia and bioconda nglview because they provide different builds
-]
+# Get all non-defaults installed packages from conda
+packages = check_output(['conda', 'list', '--json']).decode('utf8')
+packages = json.loads(packages)
 
-for channel, package in packages:
-    remote = api.package(channel, package)
+nd_packages = list()
+for package in packages:
+    if package['channel'] != 'defaults':
+        nd_packages.append(package)
+
+for package in nd_packages:
+    name = package['name']
+    channel = package['channel']
+    version = package['version']
+    build_number = package['build_number']
+
+    remote = api.package(channel, name)
     try:
-        acellera = api.package('acellera', package)
+        acellera = api.package('acellera', name)
     except:
         acellera = None
 
-    # For each file on the remove channel of the given version
+    # For each file on the remote channel of the given os, version, build, and python version
     for rf in remote['files']:
-        if (rf['version'] != remote['latest_version']) or ('py27' in rf['basename']):
+        rf_version = rf['version']
+        rf_build_number = rf['attrs']['build_number']
+        rf_build = rf['attrs']['build']
+        rf_platform = rf['attrs']['platform']
+        rf_basename = rf['basename']
+
+        if rf_version != version or rf_build_number != build_number or python_version not in rf_build or \
+                        rf_platform != osname:
             continue
 
         # Searching for the file in acellera channel
         found = False
         if acellera is not None:
             for af in acellera['files']:
-                if (af['version'] == rf['version']) and (af['basename'] == rf['basename']):
+                if af['version'] == rf_version and af['basename'] == rf['basename']:
                     found = True
                     break
 
         if not found:
-            print('\nSyncing {}/{} file "{}" to acellera'.format(channel, package, rf['basename']))
+            print('\nSyncing {}/{} file "{}" to acellera channel'.format(channel, package, rf['basename']))
 
             url = 'https:' + rf['download_url']
             print('Downloading {}'.format(url))
