b991db6ecd3f2e71913ce9de16df42d5c25ea782
Stefan Doerr <stefan.doerr@upf.edu>
2017-10-26 17:14:28 +0200

    reverted parametrization tests

---
+++ htmd/data/test-param/h2o2_esp/esp/B3LYP-cc-pVDZ-vacuum/00000/job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+trap "touch /tmp/stef/h2o2_esp/esp/B3LYP-cc-pVDZ-vacuum/00000/htmd.queues.done" EXIT SIGTERM
+
+
+trap "touch /tmp/stef/h2o2_esp/esp/B3LYP-cc-pVDZ-vacuum/00000/htmd.queues.done" EXIT SIGTERM
+
+cd /tmp/stef/h2o2_esp/esp/B3LYP-cc-pVDZ-vacuum/00000
+/tmp/stef/h2o2_esp/esp/B3LYP-cc-pVDZ-vacuum/00000/run.sh
