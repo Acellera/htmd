8b2e5c798dd30874889f6bde87d884a03b7d1313
Raimondas Galvelis <r.galvelis@acellera.com>
2017-12-21 12:41:48 +0100

    Parameterize: add more dihedral detector tests

--- htmd/parameterization/detect.py
+++ htmd/parameterization/detect.py
@@ -330,7 +330,7 @@ def detectParameterizableDihedrals(molecule):
 
     >>> import os
     >>> from htmd.home import home
-    >>> from htmd.parameterization.ffmolecule import FFMolecule
+    >>> from htmd.parameterization.ffmolecule import FFMolecule, FFTypeMethod
     >>> from htmd.parameterization.detect import detectParameterizableDihedrals
 
     Find the parameterizable dihedrals of glycol
@@ -339,23 +339,39 @@ def detectParameterizableDihedrals(molecule):
     >>> detectParameterizableDihedrals(mol)
     [[(2, 1, 0, 4), (1, 2, 3, 9)], [(0, 1, 2, 3)]]
 
-    Find the parameterizable dihedrals of chlorethene
-    >>> molFile = os.path.join(home('test-param'), 'chlorethene_1.mol2')
-    >>> mol = FFMolecule(molFile)
+    Find the parameterizable dihedrals of ethanolamine
+    >>> molFile = os.path.join(home('test-param'), 'ethanolamine.mol2')
+    >>> mol = FFMolecule(molFile, method=FFTypeMethod.GAFF2) # Does not work with CGenFF
     >>> detectParameterizableDihedrals(mol)
-    [[(2, 1, 0, 4), (2, 1, 0, 5)]]
-
-    Find the parameterizable dihedrals of chlorethene (with spaped atoms)
-    >>> molFile = os.path.join(home('test-param'), 'chlorethene_2.mol2')
-    >>> mol = FFMolecule(molFile)
-    >>> detectParameterizableDihedrals(mol)
-    [[(3, 1, 0, 4), (3, 1, 0, 5)]]
+    [[(2, 1, 0, 4)], [(0, 1, 2, 3)], [(1, 2, 3, 9), (1, 2, 3, 10)]]
 
     Find the parameterizable dihedrals of benzamidine
     >>> molFile = os.path.join(home('test-param'), 'benzamidine.mol2')
     >>> mol = FFMolecule(molFile)
     >>> detectParameterizableDihedrals(mol)
     [[(1, 0, 6, 12), (1, 0, 6, 13), (5, 0, 6, 12), (5, 0, 6, 13)], [(0, 6, 12, 16), (0, 6, 12, 17), (0, 6, 13, 14), (0, 6, 13, 15)]]
+
+    # Check if the atom swapping does not affect results
+
+    Find the parameterizable dihedrals of chlorethene
+    >>> molFile = os.path.join(home('test-param'), 'chlorethene_1.mol2')
+    >>> mol = FFMolecule(molFile)
+    >>> detectParameterizableDihedrals(mol)
+    [[(2, 1, 0, 4), (2, 1, 0, 5)]]
+
+    Find the parameterizable dihedrals of chlorethene (with swapped atoms)
+    >>> molFile = os.path.join(home('test-param'), 'chlorethene_2.mol2')
+    >>> mol = FFMolecule(molFile)
+    >>> detectParameterizableDihedrals(mol)
+    [[(3, 1, 0, 4), (3, 1, 0, 5)]]
+
+    # Check the scoring function
+
+    Find the parameterizable dihedrals of dicarbothioic acid
+    >>> molFile = os.path.join(home('test-param'), 'dicarbothioic_acid.mol2')
+    >>> mol = FFMolecule(molFile)
+    >>> detectParameterizableDihedrals(mol)
+    [[(3, 1, 0, 6)], [(0, 1, 3, 5)], [(1, 3, 5, 7)]]
     """
 
     # Get pameterizable dihedral angles
