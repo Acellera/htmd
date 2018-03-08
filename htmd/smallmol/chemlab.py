42a16c2e0e7690dfcbe4aab7dd08f11ab119373f
cuzzo87 <cuzzolin.alberto@gmail.com>
2018-03-08 17:19:27 +0100

    implemented MACSS analysis and started clustering one

--- htmd/smallmol/chemlab.py
+++ htmd/smallmol/chemlab.py
@@ -1,11 +1,18 @@
 from rdkit import Chem
+import numpy as np
 import logging
 from htmd.smallmol.util import  convertToString
 
 logger = logging.getLogger(__name__)
 
-def getMaximumCommonSubstructure(smallmol_list, returnAtomIdxs=False):
+def getMaximumCommonSubstructure(smallmol_list, removeHs=True, returnAtomIdxs=False):
     from rdkit.Chem import rdFMCS
+
+    smallmol_list = [sm.copy() for sm in smallmol_list]
+
+    if  removeHs:
+        for sm in smallmol_list:
+            sm._removeAtoms(sm.get('element H', 'idx'))
 
     rdkitMols_list = [ sm.toRdkitMol() for sm in smallmol_list]
 
@@ -31,5 +38,68 @@ def getMaximumCommonSubstructure(smallmol_list, returnAtomIdxs=False):
         atoms_no_mcs_list.append(atoms_no_mcs.tolist())
 
     return mcs_mol, atoms_mcs_list, atoms_no_mcs_list
+
+
+def cluster(smallmol_list, method, removeHs=True ):
+    from sklearn.cluster import DBSCAN  # clusterizzatore
+    import sys
+    this_module = sys.modules[__name__]
+    from rdkit.Chem.Fingerprints import FingerprintMols  # calcola path fingerprints
+    from rdkit.Chem.AtomPairs import Pairs  # Atom pairs
+    from rdkit.Chem.AtomPairs import Torsions  # Topological Torsions
+    from rdkit.Chem import AllChem  # calcola circular fingerprints
+
+    _methods = [ 'maccs','pathFingerprints','atomFingerprints','torsionsFingerprints',
+                'circularFingerprints', 'shape', 'mcs']
+    # _methods_compute = {'maccs' : MACCSkeys.GenMACCSKeys,
+    #                     'pathFingerprints' : FingerprintMols.FingerprintMol,
+    #                     'atomFingerprints' : Pairs.GetAtomPairFingerprint,
+    #                     'torsionsFingerprints':Torsions.GetTopologicalTorsionFingerprint,
+    #                     'circularFingerprints':AllChem.GetMorganFingerprint,
+    #                 }
+
+    smallmol_list = [sm.copy() for sm in smallmol_list]
+
+    if removeHs:
+        for sm in smallmol_list:
+            sm._removeAtoms(sm.get('element H', 'idx'))
+
+    rdkitMols_list = [sm.toRdkitMol() for sm in smallmol_list]
+
+    clustmethod = getattr(this_module, '_{}Clustering'.format(method))
+
+    matrix_distance = clustmethod(rdkitMols_list)
+
+    db = DBSCAN(eps=0.2, min_samples=0, metric='precomputed' ).fit(matrix_distance)
+
+    return db.labels_
+
+
+def _maccsClustering(rdkit_mols):
+    from rdkit.Chem import MACCSkeys  # calcola MACCS keys
+
+    fps = [MACCSkeys.GenMACCSKeys(m) for m in rdkit_mols ]
+
+    tanimoto_matrix = TanimotoDistances(fps)
+
+    return tanimoto_matrix
+
+def TanimotoDistances(fps):
+    from rdkit import DataStructs  # fingerprint similarity
+
+    tanimoto_matrix = []
+
+    for fp1 in fps:
+        tanimoto_row = []
+        for fp2 in fps:
+            tani = 1 - DataStructs.FingerprintSimilarity(fp1, fp2)
+            tanimoto_row.append(tani)
+        tanimoto_matrix.append(tanimoto_row)
+    tanimoto_matrix = np.array(tanimoto_matrix)
+
+    return tanimoto_matrix
+
+
+
 
 
