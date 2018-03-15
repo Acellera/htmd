42d42b3d19b2e6db3d4a1674367883cd86d6b94d
j3mdamas <jmdamas@gmail.com>
2018-03-15 12:38:42 +0100

    Pins the versions of ipywidgets and widgetsnbextension that currently work with nglview 1.0. Fixes the file which processes the dependencies to the new format Closes #584

--- package/htmd-deps/write_meta_yaml.py
+++ package/htmd-deps/write_meta_yaml.py
@@ -25,18 +25,26 @@ requirements:
 deps = []
 with open(os.path.join(workdir, 'DEPENDENCIES'), 'r') as f:
     for line in f:
-        deps.append(line.strip().lower())
+        # Remove comments and split
+        packagematch = line.split('#')[0].split()
+        # Append dict of the conda package match specification
+        if packagematch:
+            deps.append(dict(zip(['name', 'version', 'build_string'], packagematch)))
 
 # Get all installed packages from conda
 packages = check_output(['conda', 'list', '--json']).decode('utf8')
 packages = json.loads(packages)
 
 # Find the version of each dependency and add them to the meta.yaml file
-found = {key: False for key in deps}
+found = {dep['name']: False for dep in deps}
 for p in packages:
     name = p['name'].lower()
-    if name in deps:
-        text += '    - {} >={}\n'.format(name, p['version'])
+    if name in [dep['name'] for dep in deps]:
+        dep = next(dep for dep in deps if dep['name'] == name)
+        if 'version' not in dep:
+            text += '    - {} >={}\n'.format(name, p['version'])
+        else:
+            text += '    - {} {}\n'.format(name, dep['version'])
         found[name] = True
 
 # Check if all dependencies were found. If not print which and exit with error
@@ -46,5 +54,6 @@ if not all(found):
             print('Could not find dependency "{}". Exiting with error.'.format(key))
     sys.exit(1)
 
+# Write htmd-deps meta.yaml file
 with open(os.path.join(workdir, 'meta.yaml'), 'w') as f:
     f.write(text)
