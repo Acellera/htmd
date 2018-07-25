794ad8438921f0d9172c30b81f7ebd7c69fe90e7
Stefan Doerr <stefan.doerr@upf.edu>
2018-07-25 09:56:23 +0200

    fixed bug with numbers before hydrogens in element guessing code

--- htmd/molecule/readers.py
+++ htmd/molecule/readers.py
@@ -559,6 +559,7 @@ def pdbGuessElementByName(pdtopo):
     """
     from periodictable import elements
     from collections import defaultdict
+    import re
     allelements = [str(el).upper() for el in list(elements._element.values())[1:]]
 
     alternatives = defaultdict(list)
@@ -570,6 +571,7 @@ def pdbGuessElementByName(pdtopo):
 
     uqnames = np.unique(pdtopo.name[noelem])
     for name in uqnames:
+        name = re.sub('\d', ' ', name[0]) + name[1:]  # Remove numbers from first column
         elem = None
         if name[0] == ' ':  # If there is no letter in col 13 then col 14 is the element
             elem = name[1]
@@ -585,6 +587,14 @@ def pdbGuessElementByName(pdtopo):
                         alternatives[(elem, altelem)].append(name)
                     else:
                         elem = name[0] + name[1].lower()
+
+        elem = elem.strip()
+        if elem is not None and len(elem) != 0 and not elem.isalpha():
+            logger.warning('Element guessing failed for atom with name {} due to non-alphabetic characters in guessed '
+                           'element "{}". Check for incorrect column alignment in the PDB file or report to HTMD issue '
+                           'tracker.'.format(name, elem))
+            elem = None
+
         pdtopo.loc[pdtopo.name == name, 'element'] = elem
 
     for elem, altelem in alternatives:
@@ -1615,6 +1625,12 @@ class TestReaders(TestCase):
         mol = Molecule(os.path.join(self.testfolder(), 'errors.pdb'))
         refelem = np.array(['C', 'C', 'C', 'C', 'C', 'C', 'C', 'N', 'N', 'H', 'H', 'C', 'Cl', 'Ca'], dtype=object)
         assert np.array_equal(mol.element, refelem)
+
+        mol = Molecule(os.path.join(self.testfolder(), 'dialanine_solute.pdb'))
+        refelem = np.array(['H', 'C', 'H', 'H', 'C', 'O', 'N', 'H', 'C', 'H', 'C', 'H', 'H', 'H', 'C', 'O', 'N', 'H',
+                            'C', 'H', 'H', 'H'], dtype=object)
+        assert np.array_equal(mol.element, refelem)
+
 
     def test_pdb_charges(self):
         mol = Molecule(os.path.join(self.testfolder(), 'errors.pdb'))
