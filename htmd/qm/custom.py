b51656f1333d90007fb097fde1490d66da34170f
Stefan Doerr <stefan.doerr@upf.edu>
2018-08-01 10:33:12 +0200

    modified to accept custom object

--- htmd/qm/custom.py
+++ htmd/qm/custom.py
@@ -13,29 +13,36 @@ import nlopt
 
 from htmd.numbautil import dihedralAngle
 from htmd.qm.base import QMBase, QMResult
+from protocolinterface import val
 
 logger = logging.getLogger(__name__)
 
 
-class QMML(QMBase):
+class CustomCalculator(QMBase):
     """
-    Imitation of QM calculations with MM
+    Imitation of QM calculations with custom class
 
     >>> import os
     >>> import numpy as np
     >>> from tempfile import TemporaryDirectory
     >>> from htmd.home import home
     >>> from htmd.numbautil import dihedralAngle
-    >>> from htmd.parameterization.ffmolecule import FFMolecule, FFTypeMethod
-    >>> from htmd.qm.custom import QMML
+    >>> from htmd.molecule.molecule import Molecule
+    >>> from htmd.qm.custom import CustomCalculator
+    >>> from acemdai.calculator import AAICalculator
 
     Create a molecule
     >>> molFile = os.path.join(home('test-qm'), 'H2O2-90.mol2')
-    >>> mol = FFMolecule(molFile, method=FFTypeMethod.GAFF2)
+    >>> mol = Molecule(molFile)
+
+    Create the AcemdAI calculator
+    >>> networkfile = './mynet.pkl'
+    >>> aceai = AAICalculator(networkfile=networkfile, maxatoms=26, maxneighs=50)
 
     Run a single-point energy and ESP calculation
     >>> with TemporaryDirectory() as tmpDir:
-    ...     qm = QMML()
+    ...     qm = CustomCalculator()
+    ...     qm.calculator = aceai
     ...     qm.molecule = mol
     ...     qm.esp_points = np.array([[1., 1., 1.]])
     ...     qm.directory = tmpDir
@@ -44,7 +51,7 @@ class QMML(QMBase):
     CUDA: Allocating features array (1, 4, 384)
     CUDA: Allocating gradient array (1, 4, 4, 384, 3)
     >>> qm # doctest: +ELLIPSIS
-    <htmd.qm.custom.QMML object at ...>
+    <htmd.qm.custom.CustomCalculator object at ...>
     >>> result # doctest: +ELLIPSIS
     <htmd.qm.base.QMResult object at ...
     >>> result.errored
@@ -56,7 +63,8 @@ class QMML(QMBase):
 
     Run a minimization
     >>> with TemporaryDirectory() as tmpDir:
-    ...     qm = QMML()
+    ...     qm = CustomCalculator()
+    ...     qm.calculator = aceai
     ...     qm.molecule = mol
     ...     qm.optimize = True
     ...     qm.directory = tmpDir
@@ -71,7 +79,8 @@ class QMML(QMBase):
 
     Run a constrained minimization
     >>> with TemporaryDirectory() as tmpDir:
-    ...     qm = QMML()
+    ...     qm = CustomCalculator()
+    ...     qm.calculator = aceai
     ...     qm.molecule = mol
     ...     qm.optimize = True
     ...     qm.restrained_dihedrals = np.array([[2, 0, 1, 3]])
@@ -85,6 +94,11 @@ class QMML(QMBase):
     >>> np.rad2deg(dihedralAngle(result.coords[[2, 0, 1, 3], :, 0])) # doctest: +ELLIPSIS
     89.99...
     """
+
+    def __init__(self):
+        super().__init__()
+        self._arg('calculator', ':class: `Calculator`', 'Calculator object', default=None,
+                  validator=None, required=True)
 
     # Fake implementations of the abstract methods
     def _command(self): pass
@@ -113,33 +127,32 @@ class QMML(QMBase):
                 logger.info('Loading data from %s' % pickleFile)
 
             else:
-                try:
-                    from qmml import QMMLCalculator
-                except ImportError:
-                    raise ImportError('You need access to the qmml module.')
-
                 start = time.clock()
 
                 result = QMResult()
                 result.errored = False
                 result.coords = self.molecule.coords[:, :, iframe:iframe + 1].copy()
 
-                calc = QMMLCalculator(maxatoms=self.molecule.numAtoms)
-
                 if self.optimize:
                     opt = nlopt.opt(nlopt.LN_COBYLA, result.coords.size)
+
                     def objective(x, _):
-                        return float(calc.calculate(x.reshape((-1, 3, 1)), self.molecule.element)[0])
+                        return float(self.calculator.calculate(x.reshape((-1, 3, 1)), self.molecule.element)[0])
+
                     opt.set_min_objective(objective)
+
                     if self.restrained_dihedrals is not None:
                         for dihedral in self.restrained_dihedrals:
                             indices = dihedral.copy()
                             ref_angle = dihedralAngle(self.molecule.coords[indices, :, iframe])
+
                             def constraint(x, _):
                                 coords = x.reshape((-1, 3))
                                 angle = dihedralAngle(coords[indices])
                                 return np.sin(.5*(angle - ref_angle))
+
                             opt.add_equality_constraint(constraint)
+
                     opt.set_xtol_abs(1e-3) # Similar to Psi4 default
                     opt.set_maxeval(1000*opt.get_dimension())
                     opt.set_initial_step(1e-3)
@@ -151,7 +164,7 @@ class QMML(QMBase):
                     mol.coords = result.coords
                     mol.write(molFile)
 
-                result.energy = float(calc.calculate(result.coords, self.molecule.element)[0])
+                result.energy = float(self.calculator.calculate(result.coords, self.molecule.element)[0])
                 result.dipole = self.molecule.getDipole()
 
                 #if self.optimize:
@@ -159,7 +172,7 @@ class QMML(QMBase):
 
                 finish = time.clock()
                 result.calculator_time = finish - start
-                logger.info('QMML calculation time: %f s' % result.calculator_time)
+                logger.info('Custom calculator calculation time: %f s' % result.calculator_time)
 
                 with open(pickleFile, 'wb') as fd:
                     pickle.dump(result, fd)
