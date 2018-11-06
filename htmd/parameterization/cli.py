0d6aa62a88cb43b9e79b0e2bab49577309de4d4b
Raimondas Galvelis <r.galvelis@acellera.com>
2018-11-06 15:43:56 +0100

    Enable AM1-BCC

--- htmd/parameterization/cli.py
+++ htmd/parameterization/cli.py
@@ -41,7 +41,7 @@ def getArgumentParser():
                         help='QM environment (default: %(default)s)')
     parser.add_argument('--no-min', action='store_false', dest='minimize',
                         help='Do not perform QM structure minimization')
-    parser.add_argument('--charge-type', default='ESP', choices=['None', 'Gasteiger', 'ESP'],
+    parser.add_argument('--charge-type', default='ESP', choices=['None', 'Gasteiger', 'AM1-BCC', 'ESP'],
                         help='Partial atomic charge type (default: %(default)s)')
     parser.add_argument('--no-dihed', action='store_false', dest='fit_dihedral',
                         help='Do not perform QM scanning of dihedral angles')
@@ -122,7 +122,7 @@ def printReport(mol, netcharge, equivalents, all_dihedrals):
 
 def _fit_charges(mol, args, qm):
 
-    from htmd.charge import fitGasteigerCharges, fitESPCharges
+    from htmd.charge import fitGasteigerCharges, fitChargesWithAntechamber, fitESPCharges
     from htmd.parameterization.util import guessBondType, getFixedChargeAtomIndices, getDipole, _qm_method_name
 
     logger.info('=== Fitting atomic charges ===')
@@ -149,6 +149,13 @@ def _fit_charges(mol, args, qm):
         if args.charge != charge:
             raise RuntimeError('Molecular charge is set to {}, but Gasteiger atomic charges add up to {}.'.format(
                 args.charge, charge))
+
+    elif args.charge_type == 'AM1-BCC':
+
+        if len(args.fix_charge) > 0:
+            logger.warning('Flag --fix-charge does not have effect!')
+
+        mol = fitChargesWithAntechamber(mol, type='bcc', molCharge=args.charge)
 
     elif args.charge_type == 'ESP':
 
