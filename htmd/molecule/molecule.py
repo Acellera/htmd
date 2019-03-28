c0ca39c10ec59bd27daea41ab24bd3491232ebc0
Stefan Doerr <stefan.doerr@upf.edu>
2019-03-28 17:53:38 +0100

    added deprecation warning

---
+++ htmd/molecule/molecule.py
@@ -0,0 +1,4 @@
+from moleculekit.molecule import Molecule
+import warning
+
+warning('Please do not import htmd.molecule.molecule. It has been moved to the moleculekit package. This import will be deprecated.')
