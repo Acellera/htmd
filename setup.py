0316713741ecc045e619af0757dce2faf9fa753c
Stefan Doerr <stefan.doerr@upf.edu>
2019-06-14 10:36:34 +0300

    updated setup.py file

--- setup.py
+++ setup.py
@@ -4,39 +4,40 @@
 # No redistribution in whole or part
 #
 from setuptools import setup, find_packages
-import subprocess
+from subprocess import check_output
 
-version = subprocess.Popen(["git", "describe", "--tags"], stdout=subprocess.PIPE).stdout.read().decode("utf8")
-version = version.split("-")
-version = version[0]
+version = check_output(['git', 'describe', '--tags']).decode('utf8').split('-')[0]
 
-f = open("package/htmd-deps/DEPENDENCIES", "r")
-deps = None
+with open('package/htmd-deps/DEPENDENCIES', 'r') as f:
+    requirements = f.read().splitlines()
+requirements = None  # Some of them are on conda and cannot work with pip install
 
-print("Version [%s]" % (version))
-print("Dependencies:")
-print(deps)
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+print('Version [%s]' % (version))
+print('Dependencies:')
+print(requirements)
 
 setup(name='htmd',
       version=version,
       description='HTMD',
-      packages=find_packages(exclude=['data']),
-      install_requires=deps,
+      long_description=long_description,
       zip_safe=False,
-      url="https://www.htmd.org",
-      maintainer="Acellera Ltd",
-      maintainer_email="noreply@acellera.com",
+      url='https://www.htmd.org',
+      maintainer='Acellera Ltd',
+      maintainer_email='noreply@acellera.com',
       entry_points={
-          "console_scripts": [
-              'htmdnb    = htmdx.cli:main_htmd_notebook',
-              'htmd      = htmdx.cli:main_htmd',
-              'htmd_register   = htmdx.cli:main_do_nothing',
+          'console_scripts': [
+              'htmd_register   = htmdx.cli:htmd_do_register',
               'parameterize = htmd.parameterization.cli:main_parameterize',
               'activate_license  = htmdx.cli:main_activate'
           ]
       },
+
+      install_requires=requirements,
+      packages=find_packages(include=['htmd*'], exclude=['data']),
       package_data={
         'htmd': ['share/*/*/*/*'],
-      },
-      include_package_data=True
-      )
+      }
+)
