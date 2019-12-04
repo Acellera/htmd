ff2053af036f3a969083a78b6ec26b6b38a0dcac
Raimondas Galvelis <r.galvelis@acellera.com>
2019-12-04 14:38:08 +0100

    Add documentation for TeraChem

--- htmd/qm/terachem.py
+++ htmd/qm/terachem.py
@@ -12,6 +12,107 @@ from htmd.qm.base import QMBase, QMResult
 
 
 class TeraChem(QMBase):
+    """
+    Class to set up and run QM calculations with TeraChem
+
+    Capabilities
+    ------------
+    - Single-point energy calculations with HF and DFT.
+    - Electronic properties: dipole and Mulliken charges
+    - Geometry optimization with/without dihedral restraints
+
+    Attributes
+    ----------
+    molecule : :class:`Molecule`
+        Molecule
+    multiplity : int, default = 1
+        Multiplicity of the molecule (1 = singlet, 2 = doublet, 3 = triplet, etc.)
+    theory : str, default = 'BLYP3'
+        Level of theory. A full list is at Psi4.THEORIES
+    basis : str, default = '6-31G*'
+        Basis set. A full list is at Psi4.BASIS_SETS
+    correction : str, default = 'none'
+        Empirical dispersion correction. A full list is at Psi4.CORRECTIONS
+    solvent : str, default = 'vacuum'
+         Implicit solvent model. A full isit is at Psi4.SOLVENTS
+    esp_points : nunpy.ndarray, default = None
+        Point coordinates to compute ESP values. The array shape has to be (number_of_points, 3)
+    optimize : bool, default = False
+        Run geometery optimization
+    restrained_dihedrals : nunpy.ndarray, default = None
+        List of restrained dihedrals (0-based atom indices). The array shape has to be (number_of_restrained_dihedrals, 4).
+    queue : :class:`SimQueue`, defualt = :class:`LocalCPUQueue`
+        Queue object used to run simulations
+    directory : str, default = '.'
+        Working directoy
+
+    Examples
+    --------
+
+    Create an object of H2 molecule
+    >>> import os
+    >>> from htmd.home import home
+    >>> from moleculekit.molecule import Molecule
+    >>> molFile = os.path.join(home('test-qm'), 'H2-0.74.mol2')
+    >>> mol = Molecule(molFile)
+
+    Create a Psi4 object
+    >>> from htmd.qm import TeraChem
+    >>> qm = TeraChem()
+    >>> qm # doctest: +ELLIPSIS
+    <htmd.qm.terachem.TeraChem object at 0x...>
+
+    Run single-point QM calculation of H2 with BLYP and cc-pVDZ
+    >>> from tempfile import TemporaryDirectory
+    >>> with TemporaryDirectory() as tmp:
+    ...     qm.molecule = mol
+    ...     qm.theory = 'BLYP'
+    ...     qm.basis = 'cc-pVDZ'
+    ...     qm.directory = tmp
+    ...     qm.directory = '.'
+    ...     result = qm.run()
+
+    The QM results are returned as a list of htmd.qm.QMResult objects. See htmd.qm.QMResult documentation for details.
+    >>> result # doctest: +ELLIPSIS
+    [<htmd.qm.base.QMResult object at 0x...>]
+    >>> result[0].errored
+    False
+    >>> result[0].energy # doctest: +ELLIPSIS
+    -728.97068164...
+    >>> result[0].mulliken # doctest: +ELLIPSIS
+    [...0.0, ...0.0]
+
+    Run the geometry optimization of H2 with BLYP, but change basis to 3-21G.
+    NOTE: the `directory` attribut needs to be set to empty or non-existing directory, overwise the previous
+    calculation results are read.
+    >>> with TemporaryDirectory() as tmp:
+    ...     qm.basis = '3-21G'
+    ...     qm.optimize = True
+    ...     qm.directory = tmp
+    ...     result = qm.run()
+
+    The initial coordinates and optimize coordinates of H2 can be compared
+    >>> mol.coords
+    array([[[ 0.  ],
+            [ 0.  ],
+            [-0.37]],
+    <BLANKLINE>
+           [[ 0.  ],
+            [ 0.  ],
+            [ 0.37]]], dtype=float32)
+    >>> result[0].coords
+    array([[[ 0.        ],
+            [ 0.        ],
+            [-0.37542403]],
+    <BLANKLINE>
+           [[-0.        ],
+            [-0.        ],
+            [ 0.37542403]]], dtype=float32)
+
+    The QM calculations run using LocalCPUQueue by default, but this can be changed to the others.
+    >>> from jobqueues.slurmqueue import SlurmQueue
+    >>> qm.queue = SlurmQueue() # doctest: +SKIP
+    """
 
     @property
     def _command(self):
@@ -115,4 +216,6 @@ if __name__ == '__main__':
     import sys
     import doctest
 
-    sys.exit(doctest.testmod().failed)
+    # There is no TeraChem on Travis
+    if 'TRAVIS' not in os.environ:
+        sys.exit(doctest.testmod().failed)
