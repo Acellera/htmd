5520be741b28f6d2a6842d888d012d52702998e1
Stefan Doerr <stefdoerr@gmail.com>
2021-09-06 10:28:21 +0300

    added new fast test for adaptive

--- htmd/adaptive/adaptivegoal.py
+++ htmd/adaptive/adaptivegoal.py
@@ -304,7 +304,48 @@ class AdaptiveGoal(AdaptiveMD):
         return means, stds
 
 
+import unittest
+
+class _TestAdaptiveGoal(unittest.TestCase):
+    def test_adaptive_goal(self):
+        from moleculekit.projections.metricdistance import MetricDistance
+        from jobqueues.localqueue import LocalGPUQueue
+        from htmd.home import home
+        import tempfile
+        import shutil
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            print(tmpdir)
+            gendir = os.path.join(home(dataDir="test-adaptive"), "test-ions", "generators")
+
+            shutil.copytree(gendir, os.path.join(tmpdir, "generators"))
+            inpdir = os.path.join(tmpdir, "input")
+            datdir = os.path.join(tmpdir, "data")
+
+            dist = MetricDistance('name CL', 'name NA', periodic='selections')
+
+            app = LocalGPUQueue()
+            app.ngpu = 2
+            app.datadir = datdir
+
+            md = AdaptiveGoal()
+            md.nmin = 1
+            md.nmax = 2
+            md.nepochs = 2
+            md.updateperiod = 5
+            md.projection = dist
+            md.goalfunction = lambda mol: -dist.project(mol).flatten()
+            md.ticadim = 0
+            md.generatorspath = gendir
+            md.inputpath = inpdir
+            md.datapath = datdir
+            md.app = app
+            md.run()
+
+
 if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
     from moleculekit.projections.metricdistance import MetricDistance
     import htmd.home
     from moleculekit.molecule import Molecule
