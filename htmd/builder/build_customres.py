a31c641f8fbf3d3e8256ea0afc1b4041010fe5f5
Stefan Doerr <stefdoerr@gmail.com>
2021-12-03 14:26:23 +0200

    added parameterization and building functionality for non-standard residues

---
+++ htmd/builder/build_customres.py
@@ -0,0 +1,302 @@
+from parameterize.parameterization.cli import main_parameterize
+from moleculekit.tools.graphalignment import makeMolGraph, compareGraphs
+from moleculekit.molecule import Molecule
+from htmd.home import home
+import shutil
+import parmed
+import numpy as np
+import tempfile
+import os
+import types
+
+
+def get_calculator():
+    from energyforcecalculators.torchanicalculator import TorchAniCalculator
+
+    return TorchAniCalculator(model="ANI-2x")
+
+
+mymod = types.ModuleType("wrappers.torchani2x")
+mymod.get_calculator = get_calculator
+
+ace = Molecule(os.path.join(home(shareDir="builder"), "caps", "ACE.cif"))
+nme = Molecule(os.path.join(home(shareDir="builder"), "caps", "NME.cif"))
+
+
+def _cap_residue(mol):
+    mol = mol.copy()
+    resn = mol.resname[0]
+
+    acec = ace.copy()
+    acec.align("name N H CA", refmol=mol)
+    acec.remove("name N H CA", _logger=False)
+
+    nmec = nme.copy()
+    nmec.align("name CA C O", refmol=mol)
+    nmec.remove("name CA C O", _logger=False)
+
+    mol.insert(acec, index=0)
+    mol.append(nmec)
+    # Bond ACE/NME to the residue
+    mol.bonds = np.vstack(
+        (
+            mol.bonds,
+            [
+                np.where((mol.resname == "NME") & (mol.name == "N"))[0][0],
+                np.where((mol.resname == resn) & (mol.name == "C"))[0][0],
+            ],
+            [
+                np.where((mol.resname == "ACE") & (mol.name == "C"))[0][0],
+                np.where((mol.resname == resn) & (mol.name == "N"))[0][0],
+            ],
+        )
+    )
+    mol.bondtype = np.hstack((mol.bondtype, ["1"], ["1"]))
+    return mol
+
+
+def _parameterize_custom_residue(mol, outdir):
+    os.makedirs(outdir, exist_ok=True)
+
+    mol = mol.copy()
+    # Remove backbone formal charge from templated molecule
+    mol.formalcharge[mol.name == "N"] = 0
+    resn = mol.resname[0]
+    with tempfile.TemporaryDirectory() as tmpdir:
+        cmol = _cap_residue(mol)
+
+        sdffile = os.path.join(tmpdir, "mol.sdf")
+        cmol.write(sdffile)
+        # TODO: Improve it to not parameterize the cap dihedrals!
+        main_parameterize(
+            [
+                sdffile,
+                "--charge",
+                str(int(cmol.formalcharge.sum())),
+                "--nnp",
+                "wrappers.torchani2x",
+                "--charge-type",
+                "AM1-BCC",
+                "--forcefield",
+                "GAFF2",
+                "--min-type",
+                "mm",
+                "--scan-type",
+                "mm",
+                "--dihed-fit-type",
+                "iterative",
+                "--outdir",
+                tmpdir,
+            ]
+        )
+        shutil.copy(
+            os.path.join(tmpdir, "parameters", "GAFF2", "mol-orig.mol2"),
+            os.path.join(tmpdir, f"{resn}.mol2"),
+        )
+        shutil.copy(
+            os.path.join(tmpdir, "parameters", "GAFF2", "mol.frcmod"),
+            os.path.join(outdir, f"{resn}.frcmod"),
+        )
+
+        _post_process_parameterize(cmol, tmpdir, outdir, resn)
+
+
+def _post_process_parameterize(cmol, tmpdir, outdir, resn):
+    from subprocess import call
+
+    # TODO: Move this to parameterize (?)
+    mol = Molecule(os.path.join(tmpdir, f"{resn}.mol2"))
+    fields = ("element",)
+    g1 = makeMolGraph(cmol, "all", fields)
+    g2 = makeMolGraph(mol, "all", fields)
+    _, _, matching = compareGraphs(
+        g1, g2, fields=fields, tolerance=0.5, returnmatching=True
+    )
+    for pp in matching:  # Rename atoms in reference molecule and copy formal charges
+        mol.name[pp[0]] = cmol.name[pp[1]]
+        mol.formalcharge[pp[0]] = cmol.formalcharge[pp[1]]
+
+    # Remove the caps
+    mask = np.ones(mol.numAtoms, dtype=bool)
+    mask[:6] = False
+    mask[-6:] = False
+
+    mol.filter(mask, _logger=False)
+
+    # Rename backbone atom types
+    backbone_at = {"N": "N", "H": "H", "CA": "CT", "HA": "H1", "C": "C", "O": "O"}
+    original = {}
+    for key, val in backbone_at.items():
+        original[mol.atomtype[mol.name == key][0]] = backbone_at[key]
+        mol.atomtype[mol.name == key] = val
+
+    mol2file = os.path.join(tmpdir, f"{resn}.mol2")
+    mol.write(mol2file)
+
+    frcmod = os.path.join(outdir, f"{resn}.frcmod")
+    prm = parmed.amber.AmberParameterSet(frcmod)
+
+    # Duplicate parameters for renamed atoms which don't exist in the backbone
+    _duplicate_parameters(prm, original)
+
+    # Remove unused parameters
+    _clean_prm(prm, mol)
+    prm.write(frcmod)
+
+    acfile = os.path.join(tmpdir, f"{resn}_mod.ac")
+    call(
+        [
+            "antechamber",
+            "-fi",
+            "mol2",
+            "-fo",
+            "ac",
+            "-i",
+            mol2file,
+            "-o",
+            acfile,
+            "-dr",
+            "n",
+            "-j",
+            "0",
+            "-nc",
+            str(mol.formalcharge.sum()),
+        ]
+    )
+
+    # TODO: Recover the lost formal charge!!!
+    mainchain = os.path.join(tmpdir, f"mainchain.{resn.lower()}")
+    with open(mainchain, "w") as f:
+        f.write("HEAD_NAME N\n")
+        f.write("TAIL_NAME C\n")
+        f.write("MAIN_CHAIN CA\n")
+        f.write("PRE_HEAD_TYPE C\n")
+        f.write("POST_TAIL_TYPE N\n")
+        f.write(f"CHARGE {mol.formalcharge.sum():.1f}\n")
+
+    prepi = os.path.join(outdir, f"{resn}.prepi")
+    call(
+        [
+            "prepgen",
+            "-i",
+            acfile,
+            "-o",
+            prepi,
+            "-f",
+            "prepi",
+            "-m",
+            mainchain,
+            "-rn",
+            resn,
+        ]
+    )
+
+
+def _clean_prm(prm, mol):
+    from moleculekit.util import guessAnglesAndDihedrals
+
+    all_at = np.unique(mol.atomtype)
+    angles, dihedrals = guessAnglesAndDihedrals(mol.bonds)
+    bond_at = mol.atomtype[mol.bonds].tolist()
+    angle_at = mol.atomtype[angles].tolist()
+    dihed_at = mol.atomtype[dihedrals].tolist()
+
+    to_delete = []
+    for at in prm.atom_types:
+        if at not in all_at.tolist():
+            to_delete.append(at)
+    for at in to_delete:
+        del prm.atom_types[at]
+
+    to_delete = []
+    for bt in prm.bond_types:
+        if not np.any(np.isin(bt, all_at)):
+            to_delete.append(bt)
+        elif list(bt) not in bond_at and list(bt)[::-1] not in bond_at:
+            to_delete.append(bt)
+    for bt in to_delete:
+        del prm.bond_types[bt]
+
+    to_delete = []
+    for bt in prm.angle_types:
+        if not np.any(np.isin(bt, all_at)):
+            to_delete.append(bt)
+        elif list(bt) not in angle_at and list(bt)[::-1] not in angle_at:
+            to_delete.append(bt)
+    for bt in to_delete:
+        del prm.angle_types[bt]
+
+    to_delete = []
+    for bt in prm.dihedral_types:
+        if not np.any(np.isin(bt, all_at)):
+            to_delete.append(bt)
+        elif list(bt) not in dihed_at and list(bt)[::-1] not in dihed_at:
+            to_delete.append(bt)
+    for bt in to_delete:
+        del prm.dihedral_types[bt]
+
+    to_delete = []
+    for bt in prm.improper_types:
+        if not np.any(np.isin(bt, all_at)):
+            to_delete.append(bt)
+    for bt in to_delete:
+        del prm.improper_types[bt]
+
+
+def _duplicate_parameters(prm, original):
+    # Duplicate parameters for renamed atoms which don't exist in the backbone
+    new_bt = {}
+    for bt in prm.bond_types:
+        bt = np.array(bt)
+        # If one of the two atom types was replaced duplicate the param
+        replaced = np.isin(bt, list(original.keys()))
+        if replaced.sum() != 1:
+            continue
+        bt_new = bt.copy()
+        bt_new[replaced] = [original[val] for val in bt[replaced]]
+        new_bt[tuple(bt_new)] = prm.bond_types[tuple(bt)]
+
+    for bt in new_bt:
+        prm.bond_types[bt] = new_bt[bt]
+
+    new_bt = {}
+    for bt in prm.angle_types:
+        bt = np.array(bt)
+        # If one of the two atom types was replaced duplicate the param
+        replaced = np.isin(bt, list(original.keys()))
+        if replaced.sum() == 0 or replaced.sum() == 3:
+            continue
+        bt_new = bt.copy()
+        bt_new[replaced] = [original[val] for val in bt[replaced]]
+        new_bt[tuple(bt_new)] = prm.angle_types[tuple(bt)]
+
+    for bt in new_bt:
+        prm.angle_types[bt] = new_bt[bt]
+
+    new_bt = {}
+    for bt in prm.dihedral_types:
+        bt = np.array(bt)
+        # If one of the two atom types was replaced duplicate the param
+        replaced = np.isin(bt, list(original.keys()))
+        if replaced.sum() == 0 or replaced.sum() == 4:
+            continue
+        bt_new = bt.copy()
+        bt_new[replaced] = [original[val] for val in bt[replaced]]
+        new_bt[tuple(bt_new)] = prm.dihedral_types[tuple(bt)]
+
+    for bt in new_bt:
+        prm.dihedral_types[bt] = new_bt[bt]
+
+    new_bt = {}
+    for bt in prm.improper_types:
+        bt = np.array(bt)
+        # If one of the two atom types was replaced duplicate the param
+        replaced = np.isin(bt, list(original.keys()))
+        if replaced.sum() == 0 or replaced.sum() == 4:
+            continue
+        bt_new = bt.copy()
+        bt_new[replaced] = [original[val] for val in bt[replaced]]
+        new_bt[tuple(bt_new)] = prm.improper_types[tuple(bt)]
+
+    for bt in new_bt:
+        prm.improper_types[bt] = new_bt[bt]
