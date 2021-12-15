c122ceda622babb93eb6a20e9a55459fb61a9081
Stefan Doerr <stefdoerr@gmail.com>
2021-12-15 14:17:17 +0200

    Update headers (#1018)
    * updated all license headers
    
    * fix

--- htmd/queues/pbsqueue.py
+++ htmd/queues/pbsqueue.py
@@ -1,3 +1,8 @@
+# (c) 2015-2022 Acellera Ltd http://www.acellera.com
+# All Rights Reserved
+# Distributed under HTMD Software License Agreement
+# No redistribution in whole or part
+#
 from jobqueues.pbsqueue import PBSQueue
 import logging
 
