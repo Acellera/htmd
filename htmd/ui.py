cc867c88986185b3fc7d18b0322228e4ea596379
Stefan Doerr <stefdoerr@gmail.com>
2026-05-28 19:01:53 +0300

    removed depreacted import

--- htmd/ui.py
+++ htmd/ui.py
@@ -57,10 +57,7 @@ from moleculekit.vmdgraphics import (
 from moleculekit.tools.autosegment import autoSegment
 from htmd.builder.loopmodeler import loopModeller
 
-try:
-    from moleculekit.tools.preparation import systemPrepare, proteinPrepare
-except Exception as e:
-    warnings.warn(f"{e}")
+from moleculekit.tools.preparation import systemPrepare
 
 from moleculekit.tools.nonstandard_residues import detectNonStandardResidues
 from htmd.builder.nonstandard import parameterizeFromSpecs
